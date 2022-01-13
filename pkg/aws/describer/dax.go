beaa4bb2f11dad449dd1f0fc94755ff21fb7a5e4
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-01-13 20:00:15 +0330

    Fixes dax

--- pkg/aws/describer/dax.go
+++ pkg/aws/describer/dax.go
@@ -2,6 +2,7 @@ package describer
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dax"
@@ -17,6 +18,11 @@ func DAXCluster(ctx context.Context, cfg aws.Config) ([]Resource, error) {
 	client := dax.NewFromConfig(cfg)
 	out, err := client.DescribeClusters(ctx, &dax.DescribeClustersInput{})
 	if err != nil {
+		// Dax Cluster is not available in all regions yet and throws exceptions for those regions
+		// https://aws.amazon.com/about-aws/whats-new/2018/04/amazon-dynamodb-accelerator-regional-expansion/
+		if strings.Contains(err.Error(), "InvalidParameterValueException") || strings.Contains(err.Error(), "no such host") {
+			return nil, nil
+		}
 		return nil, err
 	}
 
@@ -26,7 +32,11 @@ func DAXCluster(ctx context.Context, cfg aws.Config) ([]Resource, error) {
 			ResourceName: cluster.ClusterArn,
 		})
 		if err != nil {
-			return nil, err
+			if strings.Contains(err.Error(), "ClusterNotFoundFault") {
+				tags = nil
+			} else {
+				return nil, err
+			}
 		}
 
 		values = append(values, Resource{
