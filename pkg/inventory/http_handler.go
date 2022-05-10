654b0c20691ccbe3583ed2663639af2263237644
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-05-10 19:43:45 +0430

    feat: Adds change credentials

--- pkg/inventory/http_handler.go
+++ pkg/inventory/http_handler.go
@@ -2,6 +2,11 @@ package inventory
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/gorm/logger"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -45,8 +50,19 @@ func InitializeHttpHandler(
 		postgresPassword,
 		postgresDb,
 	)
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		return nil, err
 	}
