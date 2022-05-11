d1741faea0b7c249f92029b80c178036194c6899
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-05-11 12:01:36 +0430

    fix: Removes db query logger

--- pkg/inventory/http_handler.go
+++ pkg/inventory/http_handler.go
@@ -2,12 +2,6 @@ package inventory
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"time"
-
-	"gorm.io/gorm/logger"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
@@ -50,19 +44,8 @@ func InitializeHttpHandler(
 		postgresPassword,
 		postgresDb,
 	)
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			Colorful:                  true,        // Disable color
-		},
-	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: newLogger,
-	})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
