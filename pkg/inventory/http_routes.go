e914cd6cb572dccfd5a4e062935968afcf7ac2ab
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-06-13 17:59:43 +0430

    fix: Fixes get locations

--- pkg/inventory/http_routes.go
+++ pkg/inventory/http_routes.go
@@ -2065,7 +2065,7 @@ func (h *HttpHandler) GetLocations(ctx echo.Context) error {
 			return err
 		}
 
-		var res map[string]interface{}
+		res := map[string]interface{}{}
 		for regions.HasNext() {
 			regions, err := regions.NextPage(ctx.Request().Context())
 			if err != nil {
