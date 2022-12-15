8d9a6bf2afc8f2efc23f7256acbfd10f89fdc14c
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-12-15 20:54:54 +0330

    fix: Fixes auth0 users by workspace

--- pkg/auth/auth0/entities.go
+++ pkg/auth/auth0/entities.go
@@ -20,32 +20,31 @@ type TokenResponse struct {
 	TokenType   string `json:"token_type"`
 }
 
-type UserMetadata struct {
+type Metadata struct {
 	Access map[string]api.Role `json:"access"`
 }
 
-type GetUserResponse struct {
+type User struct {
 	CreatedAt     time.Time `json:"created_at"`
 	Email         string    `json:"email"`
 	EmailVerified bool      `json:"email_verified"`
 	FamilyName    string    `json:"family_name"`
 	GivenName     string    `json:"given_name"`
+	Locale        string    `json:"locale"`
 	Identities    []struct {
-		Provider    string `json:"provider"`
-		AccessToken string `json:"access_token"`
-		ExpiresIn   int    `json:"expires_in"`
-		UserId      string `json:"user_id"`
-		Connection  string `json:"connection"`
-		IsSocial    bool   `json:"isSocial"`
+		Connection string `json:"connection"`
+		Provider   string `json:"provider"`
+		UserId     string `json:"user_id"`
+		IsSocial   bool   `json:"isSocial"`
 	} `json:"identities"`
-	Locale       string       `json:"locale"`
-	Name         string       `json:"name"`
-	Nickname     string       `json:"nickname"`
-	Picture      string       `json:"picture"`
-	UpdatedAt    time.Time    `json:"updated_at"`
-	UserId       string       `json:"user_id"`
-	UserMetadata UserMetadata `json:"user_metadata"`
-	LastIp       string       `json:"last_ip"`
-	LastLogin    time.Time    `json:"last_login"`
-	LoginsCount  int          `json:"logins_count"`
+	Name         string    `json:"name"`
+	Nickname     string    `json:"nickname"`
+	Picture      string    `json:"picture"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	UserId       string    `json:"user_id"`
+	UserMetadata Metadata  `json:"user_metadata"`
+	LastLogin    time.Time `json:"last_login"`
+	LastIp       string    `json:"last_ip"`
+	LoginsCount  int       `json:"logins_count"`
+	AppMetadata  Metadata  `json:"app_metadata"`
 }
