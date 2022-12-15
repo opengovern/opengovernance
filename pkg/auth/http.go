66fe204c8a7595e4d79527fbe1d5830ea3806ba7
Saleh Khazaei <saleh.khazaei@gmail.com>
2022-12-15 20:34:39 +0330

    fix: Uses auth0 as source of truth for user binding

--- pkg/auth/http.go
+++ pkg/auth/http.go
@@ -1,27 +1,19 @@
 package auth
 
 import (
-	"errors"
-	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"gitlab.com/keibiengine/keibi-engine/pkg/auth/auth0"
 
-	"gitlab.com/keibiengine/keibi-engine/pkg/internal/httpclient"
 	"gitlab.com/keibiengine/keibi-engine/pkg/workspace/client"
 
 	"gitlab.com/keibiengine/keibi-engine/pkg/internal/email"
 
-	"gitlab.com/keibiengine/keibi-engine/pkg/auth/emails"
-
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"gitlab.com/keibiengine/keibi-engine/pkg/auth/api"
 	"gitlab.com/keibiengine/keibi-engine/pkg/internal/httpserver"
 	"go.uber.org/zap"
-	"gorm.io/gorm"
 )
 
 const inviteDuration = time.Hour * 24 * 7
@@ -68,50 +60,52 @@ func bindValidate(ctx echo.Context, i interface{}) error {
 // @Param       role   body     string true "role"
 // @Router      /auth/api/v1/role/binding [put]
 func (r httpRoutes) PutRoleBinding(ctx echo.Context) error {
-	var req api.PutRoleBindingRequest
-	if err := bindValidate(ctx, &req); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err)
-	}
-
-	// The WorkspaceManager service will call this API to set the AdminRole
-	// for the admin user on behalf of him. Allow for the Admin to only set its
-	// role to admin for that user case
-	if httpserver.GetUserID(ctx) == req.UserID &&
-		req.Role != api.AdminRole {
-		return echo.NewHTTPError(http.StatusBadRequest, "admin user permission can't be modified by self")
-	}
-
-	workspaceName := httpserver.GetWorkspaceName(ctx)
-	count, err := r.db.CountRoleBindings(workspaceName)
-	if err != nil {
-		return err
-	}
-
-	limits, err := r.workspaceClient.GetLimits(httpclient.FromEchoContext(ctx), true)
-	if err != nil {
-		return err
-	}
-
-	if count >= limits.MaxUsers {
-		err = r.db.UpdateRoleBinding(&RoleBinding{
-			UserID:        req.UserID,
-			WorkspaceName: workspaceName,
-			Role:          req.Role,
-			AssignedAt:    time.Now(),
-		})
-	} else {
-		err = r.db.CreateOrUpdateRoleBinding(&RoleBinding{
-			UserID:        req.UserID,
-			WorkspaceName: workspaceName,
-			Role:          req.Role,
-			AssignedAt:    time.Now(),
-		})
-	}
-	if err != nil {
-		return err
-	}
-
-	return ctx.NoContent(http.StatusOK)
+	return echo.NewHTTPError(http.StatusNotImplemented)
+	//
+	//var req api.PutRoleBindingRequest
+	//if err := bindValidate(ctx, &req); err != nil {
+	//	return echo.NewHTTPError(http.StatusBadRequest, err)
+	//}
+	//
+	//// The WorkspaceManager service will call this API to set the AdminRole
+	//// for the admin user on behalf of him. Allow for the Admin to only set its
+	//// role to admin for that user case
+	//if httpserver.GetUserID(ctx) == req.UserID &&
+	//	req.Role != api.AdminRole {
+	//	return echo.NewHTTPError(http.StatusBadRequest, "admin user permission can't be modified by self")
+	//}
+	//
+	//workspaceName := httpserver.GetWorkspaceName(ctx)
+	//count, err := r.db.CountRoleBindings(workspaceName)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//limits, err := r.workspaceClient.GetLimits(httpclient.FromEchoContext(ctx), true)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//if count >= limits.MaxUsers {
+	//	err = r.db.UpdateRoleBinding(&RoleBinding{
+	//		UserID:        req.UserID,
+	//		WorkspaceName: workspaceName,
+	//		Role:          req.Role,
+	//		AssignedAt:    time.Now(),
+	//	})
+	//} else {
+	//	err = r.db.CreateOrUpdateRoleBinding(&RoleBinding{
+	//		UserID:        req.UserID,
+	//		WorkspaceName: workspaceName,
+	//		Role:          req.Role,
+	//		AssignedAt:    time.Now(),
+	//	})
+	//}
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//return ctx.NoContent(http.StatusOK)
 }
 
 // GetRoleBindings godoc
@@ -127,26 +121,13 @@ func (r *httpRoutes) GetRoleBindings(ctx echo.Context) error {
 		return err
 	}
 
+	var resp api.GetRoleBindingsResponse
+
 	usr, err := r.auth0Service.GetUser(user.ExternalID)
 	if err != nil {
 		r.logger.Warn("failed to get user from auth0 due to", zap.Error(err))
-		//return err
-	}
-
-	rbs, err := r.db.GetRoleBindingsOfUser(httpserver.GetUserID(ctx))
-	if err != nil {
 		return err
 	}
-
-	resp := make(api.GetRoleBindingsResponse, 0, len(rbs))
-	for _, rb := range rbs {
-		resp = append(resp, api.RoleBinding{
-			WorkspaceName: rb.WorkspaceName,
-			Role:          rb.Role,
-			AssignedAt:    rb.AssignedAt,
-		})
-	}
-
 	if usr != nil {
 		for wsName, role := range usr.UserMetadata.Access {
 			resp = append(resp, api.RoleBinding{
@@ -166,26 +147,26 @@ func (r *httpRoutes) GetRoleBindings(ctx echo.Context) error {
 // @Success     200 {object} api.GetWorkspaceRoleBindingResponse
 // @Router      /auth/api/v1/workspace/role/bindings [get]
 func (r *httpRoutes) GetWorkspaceRoleBindings(ctx echo.Context) error {
-	rbs, err := r.db.GetRoleBindingsOfWorkspace(httpserver.GetWorkspaceName(ctx))
-	if err != nil {
-		return err
-	}
-
-	resp := make(api.GetWorkspaceRoleBindingResponse, 0, len(rbs))
-	for _, rb := range rbs {
-		u, err := r.db.GetUserByID(rb.UserID)
-		if err != nil {
-			return err
-		}
-
-		resp = append(resp, api.WorkspaceRoleBinding{
-			UserID:     rb.UserID,
-			Email:      u.Email,
-			Role:       rb.Role,
-			AssignedAt: rb.AssignedAt,
-		})
-	}
-
+	//rbs, err := r.db.GetRoleBindingsOfWorkspace(httpserver.GetWorkspaceName(ctx))
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//resp := make(api.GetWorkspaceRoleBindingResponse, 0, len(rbs))
+	//for _, rb := range rbs {
+	//	u, err := r.db.GetUserByID(rb.UserID)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	resp = append(resp, api.WorkspaceRoleBinding{
+	//		UserID:     rb.UserID,
+	//		Email:      u.Email,
+	//		Role:       rb.Role,
+	//		AssignedAt: rb.AssignedAt,
+	//	})
+	//}
+	var resp api.GetWorkspaceRoleBindingResponse
 	return ctx.JSON(http.StatusOK, resp)
 }
 
@@ -196,54 +177,56 @@ func (r *httpRoutes) GetWorkspaceRoleBindings(ctx echo.Context) error {
 // @Success 200 {object} api.InviteResponse
 // @Router  /auth/api/v1/invite [post]
 func (r *httpRoutes) Invite(ctx echo.Context) error {
-	var req api.InviteRequest
-	if err := bindValidate(ctx, &req); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err)
-	}
+	return echo.NewHTTPError(http.StatusNotImplemented)
 
-	req.Email = strings.ToLower(strings.TrimSpace(req.Email))
-
-	workspaceName := httpserver.GetWorkspaceName(ctx)
-
-	inv := Invitation{
-		Email:         req.Email,
-		ExpiredAt:     time.Now().UTC().Add(inviteDuration),
-		WorkspaceName: workspaceName,
-	}
-
-	count, err := r.db.CountRoleBindings(workspaceName)
-	if err != nil {
-		return err
-	}
-
-	limits, err := r.workspaceClient.GetLimits(httpclient.FromEchoContext(ctx), true)
-	if err != nil {
-		return err
-	}
-
-	if count >= limits.MaxUsers {
-		return echo.NewHTTPError(http.StatusBadRequest, "user limit reached")
-	}
-
-	err = r.db.CreateInvitation(&inv)
-	if err != nil {
-		return err
-	}
-
-	invLink := fmt.Sprintf(r.inviteLinkTemplate, inv.ID.String())
-	mBody, err := emails.GetInviteMailBody(invLink, workspaceName)
-	if err != nil {
-		return err
-	}
-
-	err = r.emailService.SendEmail(ctx.Request().Context(), req.Email, mBody)
-	if err != nil {
-		return err
-	}
-
-	return ctx.JSON(http.StatusOK, api.InviteResponse{
-		InviteID: inv.ID,
-	})
+	//var req api.InviteRequest
+	//if err := bindValidate(ctx, &req); err != nil {
+	//	return echo.NewHTTPError(http.StatusBadRequest, err)
+	//}
+	//
+	//req.Email = strings.ToLower(strings.TrimSpace(req.Email))
+	//
+	//workspaceName := httpserver.GetWorkspaceName(ctx)
+	//
+	//inv := Invitation{
+	//	Email:         req.Email,
+	//	ExpiredAt:     time.Now().UTC().Add(inviteDuration),
+	//	WorkspaceName: workspaceName,
+	//}
+	//
+	//count, err := r.db.CountRoleBindings(workspaceName)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//limits, err := r.workspaceClient.GetLimits(httpclient.FromEchoContext(ctx), true)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//if count >= limits.MaxUsers {
+	//	return echo.NewHTTPError(http.StatusBadRequest, "user limit reached")
+	//}
+	//
+	//err = r.db.CreateInvitation(&inv)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//invLink := fmt.Sprintf(r.inviteLinkTemplate, inv.ID.String())
+	//mBody, err := emails.GetInviteMailBody(invLink, workspaceName)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//err = r.emailService.SendEmail(ctx.Request().Context(), req.Email, mBody)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//return ctx.JSON(http.StatusOK, api.InviteResponse{
+	//	InviteID: inv.ID,
+	//})
 }
 
 // ListInvites godoc
@@ -253,23 +236,23 @@ func (r *httpRoutes) Invite(ctx echo.Context) error {
 // @Success 200 {object} []api.InviteItem
 // @Router  /auth/api/v1/invites [get]
 func (r *httpRoutes) ListInvites(ctx echo.Context) error {
-	workspaceName := httpserver.GetWorkspaceName(ctx)
-	invs, err := r.db.ListInvitesByWorkspaceName(workspaceName)
-	if err != nil {
-		return err
-	}
+	//workspaceName := httpserver.GetWorkspaceName(ctx)
+	//invs, err := r.db.ListInvitesByWorkspaceName(workspaceName)
+	//if err != nil {
+	//	return err
+	//}
 
 	var resp []api.InviteItem
-	for _, inv := range invs {
-		if inv.ExpiredAt.Before(time.Now()) {
-			continue
-		}
-
-		resp = append(resp, api.InviteItem{
-			Email: inv.Email,
-		})
-	}
-
+	//for _, inv := range invs {
+	//	if inv.ExpiredAt.Before(time.Now()) {
+	//		continue
+	//	}
+	//
+	//	resp = append(resp, api.InviteItem{
+	//		Email: inv.Email,
+	//	})
+	//}
+	//
 	return ctx.JSON(http.StatusOK, resp)
 }
 
@@ -280,56 +263,58 @@ func (r *httpRoutes) ListInvites(ctx echo.Context) error {
 // @Success 200 {object} nil
 // @Router  /auth/api/v1/invite/invite_id [get]
 func (r *httpRoutes) AcceptInvitation(ctx echo.Context) error {
-	invIDPrm := ctx.Param("invite_id")
-	if invIDPrm == "" {
-		return echo.NewHTTPError(http.StatusBadRequest, errors.New("empty invitation id"))
-	}
-
-	invID, err := uuid.Parse(invIDPrm)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("bad invitation id: %w", err))
-	}
-
-	// check that invitation exists
-	inv, err := r.db.GetInvitationByID(invID)
-	if err != nil {
-		r.logger.Error("invitation not found",
-			zap.String("path", ctx.Path()),
-			zap.String("method", ctx.Request().Method),
-			zap.Error(err))
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return echo.NewHTTPError(http.StatusBadRequest, "invitation not found")
-		}
-
-		return err
-	}
-
-	if inv.ExpiredAt.Before(time.Now()) {
-		r.logger.Error("invitation expired",
-			zap.String("path", ctx.Path()),
-			zap.String("method", ctx.Request().Method),
-			zap.String("invitationID", invIDPrm))
-		return echo.NewHTTPError(http.StatusBadRequest, "invitation expired")
-	}
-
-	userID := httpserver.GetUserID(ctx)
-
-	// if binding exists do not change Role
-	err = r.db.CreateBindingIfNotExists(&RoleBinding{
-		UserID:        userID,
-		WorkspaceName: inv.WorkspaceName,
-		Role:          api.ViewerRole,
-		AssignedAt:    time.Now(),
-	})
-	if err != nil {
-		return err
-	}
-
-	err = r.db.DeleteInvitation(inv.ID)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return echo.NewHTTPError(http.StatusNotImplemented)
+	//
+	//invIDPrm := ctx.Param("invite_id")
+	//if invIDPrm == "" {
+	//	return echo.NewHTTPError(http.StatusBadRequest, errors.New("empty invitation id"))
+	//}
+	//
+	//invID, err := uuid.Parse(invIDPrm)
+	//if err != nil {
+	//	return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("bad invitation id: %w", err))
+	//}
+	//
+	//// check that invitation exists
+	//inv, err := r.db.GetInvitationByID(invID)
+	//if err != nil {
+	//	r.logger.Error("invitation not found",
+	//		zap.String("path", ctx.Path()),
+	//		zap.String("method", ctx.Request().Method),
+	//		zap.Error(err))
+	//
+	//	if errors.Is(err, gorm.ErrRecordNotFound) {
+	//		return echo.NewHTTPError(http.StatusBadRequest, "invitation not found")
+	//	}
+	//
+	//	return err
+	//}
+	//
+	//if inv.ExpiredAt.Before(time.Now()) {
+	//	r.logger.Error("invitation expired",
+	//		zap.String("path", ctx.Path()),
+	//		zap.String("method", ctx.Request().Method),
+	//		zap.String("invitationID", invIDPrm))
+	//	return echo.NewHTTPError(http.StatusBadRequest, "invitation expired")
+	//}
+	//
+	//userID := httpserver.GetUserID(ctx)
+	//
+	//// if binding exists do not change Role
+	//err = r.db.CreateBindingIfNotExists(&RoleBinding{
+	//	UserID:        userID,
+	//	WorkspaceName: inv.WorkspaceName,
+	//	Role:          api.ViewerRole,
+	//	AssignedAt:    time.Now(),
+	//})
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//err = r.db.DeleteInvitation(inv.ID)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//return nil
 }
