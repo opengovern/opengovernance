eae1ee03e9f6d9cb9e1e069f673db6e8fca0fc86
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-02-28 10:45:29 +0300

    feat: Adds catalog page

---
+++ pkg/onboard/api/catalog.go
@@ -0,0 +1,24 @@
+package api
+
+type ConnectorState = string
+
+const (
+	ConnectorState_Active   ConnectorState = "ACTIVE"
+	ConnectorState_NotSetup ConnectorState = "NOT_SETUP"
+)
+
+type CatalogMetrics struct {
+	ConnectionsEnabled   int `json:"connectionsEnabled"`
+	HealthyConnections   int `json:"healthyConnections"`
+	UnhealthyConnections int `json:"unhealthyConnections"`
+	ResourcesDiscovered  int `json:"resourcesDiscovered"`
+}
+
+type CatalogConnector struct {
+	Logo            string         `json:"logo"`
+	Name            string         `json:"name"`
+	Category        string         `json:"category"`
+	Description     string         `json:"description"`
+	ConnectionCount int            `json:"connectionCount"`
+	State           ConnectorState `json:"state"`
+}
