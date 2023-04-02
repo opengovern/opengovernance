e5014bf82cb8b8b6ca86ba1e7ea3bd6ed03dc76c
saberkh10 <saber.khazaei10@gmail.com>
2023-04-02 23:34:39 +0330

    feat: Add cli2

--- cmd/cli/cmd/root.go
+++ cmd/cli/cmd/root.go
@@ -11,7 +11,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "keibi-engine",
+	Use:   "kctl",
 	Short: "test 2 ",
 	Long:  `test 2 `,
 	// Uncomment the following line if your bare application
