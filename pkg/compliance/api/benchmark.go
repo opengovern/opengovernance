bdc04da51ca111f999aff969e56a73dd8728808f
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-04-03 10:21:38 +0300

    fix: Fixes benchmarks

--- pkg/compliance/api/benchmark.go
+++ pkg/compliance/api/benchmark.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"gitlab.com/keibiengine/keibi-engine/pkg/compliance/db"
 	"gitlab.com/keibiengine/keibi-engine/pkg/source"
 	"gitlab.com/keibiengine/keibi-engine/pkg/types"
 	"time"
@@ -25,57 +23,6 @@ type Benchmark struct {
 	Policies    []string          `json:"policies"`
 	CreatedAt   time.Time         `json:"createdAt"`
 	UpdatedAt   time.Time         `json:"updatedAt"`
-}
-
-func (b *Benchmark) PopulateConnectors(db db.Database) error {
-	if len(b.Connectors) > 0 {
-		return nil
-	}
-
-	for _, childId := range b.Children {
-		child, err := db.GetBenchmark(childId)
-		if err != nil {
-			return err
-		}
-		if child == nil {
-			return fmt.Errorf("child %s not found", childId)
-		}
-
-		ca := child.ToApi()
-		err = ca.PopulateConnectors(db)
-		if err != nil {
-			return err
-		}
-
-		b.Connectors = append(b.Connectors, ca.Connectors...)
-	}
-
-	for _, policyId := range b.Policies {
-		policy, err := db.GetPolicy(policyId)
-		if err != nil {
-			return err
-		}
-		if policy == nil {
-			return fmt.Errorf("policy %s not found", policyId)
-		}
-
-		query, err := db.GetQuery(*policy.QueryID)
-		if err != nil {
-			return err
-		}
-		if query == nil {
-			return fmt.Errorf("query %s not found", *policy.QueryID)
-		}
-
-		ty, err := source.ParseType(query.Connector)
-		if err != nil {
-			return err
-		}
-
-		b.Connectors = append(b.Connectors, ty)
-	}
-
-	return nil
 }
 
 type Policy struct {
