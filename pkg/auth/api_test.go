11ba198273a8d9f09211485962b91696a7bf5ee8
artaasadi <arta-c@kaytu.io>
2023-04-11 18:50:41 +0330

    test Writing and Mocks

---
+++ pkg/auth/api_test.go
@@ -0,0 +1,183 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/suite"
+	"gitlab.com/keibiengine/keibi-engine/pkg/auth/api"
+	"gitlab.com/keibiengine/keibi-engine/pkg/auth/auth0"
+)
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, &testSuite{})
+}
+
+type testSuite struct {
+	suite.Suite
+	testServer *httptest.Server
+	service    *auth0.Service
+	httpRoutes httpRoutes
+}
+
+func (ts *testSuite) FetchData() (error, string) {
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/idrandom", ts.testServer.URL), nil)
+	if err != nil {
+		return err, ""
+	}
+
+	resp, err := ts.testServer.Client().Do(req)
+	if err != nil {
+		return err, ""
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	// b, err := ioutil.ReadAll(resp.Body)  Go.1.15 and earlier
+	if err != nil {
+		return err, ""
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%w. %s", err, http.StatusText(resp.StatusCode)), "Not OK"
+	}
+
+	return nil, string(b)
+}
+
+func (ts *testSuite) SetupSuite() {
+	ts.testServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			mockDeleteUser(w, r)
+		}
+		if r.Method == http.MethodPost {
+			mockFillTocken(w, r)
+		}
+		if r.Method == http.MethodGet {
+			mockGetUsers(w, r)
+		}
+	}))
+
+	ts.service = auth0.New(ts.testServer.URL, "test_auth0ClientIDNative", "test_auth0ClientID", "test_auth0ManageClientID",
+		"test_auth0ManageClientSecret", "test_auth0Connection", int(1))
+	ts.httpRoutes = httpRoutes{
+		auth0Service: ts.service,
+	}
+	e := echo.New()
+	ts.httpRoutes.Register(e)
+
+}
+
+func (ts *testSuite) TearDownSuite() {
+}
+
+func (ts *testSuite) TearDownTest() {
+}
+
+func (ts *testSuite) TestDeleteUser() {
+	//err, result := ts.FetchData()
+	//ts.NoError(err)
+	//ts.Equal("Welcome to the homepage!", result)
+	ts.Equal(nil, ts.service.DeleteUser("asdasd"))
+	fmt.Println(ts.service)
+}
+
+func (ts *testSuite) TestGetUser() {
+	getUserTestCases := []struct {
+		UserId   string
+		Response string
+		Error    int
+	}{
+		{
+			UserId:   "test1",
+			Response: "user1@test.com",
+		},
+		{
+			UserId: "dontHave",
+			Error:  http.StatusNoContent,
+		},
+	}
+	for i, tc := range getUserTestCases {
+		ts.T().Run(fmt.Sprintf("getUserTestCases-%d", i), func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			r.Header.Set("Content-Type", "application/json; charset=utf8")
+			w := httptest.NewRecorder()
+
+			c := echo.New().NewContext(r, w)
+			c.SetPath("/auth/api/v1/user/:user_id")
+			c.SetParamNames("user_id")
+			c.SetParamValues(tc.UserId)
+
+			err := ts.httpRoutes.GetUserDetails(c)
+			if err != nil {
+				ts.Equal("[GetUser] invalid status code: 204, body=", err.Error())
+				return
+			}
+
+			var response api.WorkspaceRoleBinding
+			if err := json.NewDecoder(w.Body).Decode(&response); err != nil {
+				ts.T().Fatalf("json decode: %v", err)
+			}
+			fmt.Println(response)
+			ts.Equal(tc.Response, response.Email)
+		})
+	}
+
+}
+
+func (ts *testSuite) TestGetRoleUsers() {
+	getUserTestCases := []struct {
+		Role     api.Role
+		Response string
+		Error    int
+	}{
+		{
+			Role:     api.AdminRole,
+			Response: "user1@test.com",
+		},
+		{
+			Role:     api.EditorRole,
+			Response: "user1@test.com",
+		},
+		{
+			Role:     api.ViewerRole,
+			Response: "user1@test.com",
+		},
+		{
+			Role:     api.KeibiAdminRole,
+			Response: "",
+		},
+	}
+	for i, tc := range getUserTestCases {
+		ts.T().Run(fmt.Sprintf("getRoleUsersTestCases-%d", i), func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			r.Header.Set("Content-Type", "application/json; charset=utf8")
+			w := httptest.NewRecorder()
+
+			c := echo.New().NewContext(r, w)
+
+			c.SetPath("/auth/api/v1/role/:role/users")
+			c.SetParamNames("role")
+			c.SetParamValues(string(tc.Role))
+
+			err := ts.httpRoutes.GetRoleUsers(c)
+			if err != nil {
+				ts.Equal("[SearchUsersByRole] invalid status code: 204, body=", err.Error())
+				return
+			}
+
+			var response api.GetRoleUsersResponse
+			if err := json.NewDecoder(w.Body).Decode(&response); err != nil {
+				ts.T().Fatalf("json decode: %v", err)
+			}
+			fmt.Println("the response is: ", response)
+			ts.Equal(tc.Role, response[0].Role)
+		})
+	}
+}
---
+++ pkg/auth/auth0_mock.go
@@ -0,0 +1,208 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"gitlab.com/keibiengine/keibi-engine/pkg/auth/api"
+	"gitlab.com/keibiengine/keibi-engine/pkg/auth/auth0"
+)
+
+type tokenStruct struct {
+	AccessToken string
+}
+
+var testUsers = []auth0.User{
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test1",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws1": api.AdminRole, "ws2": api.EditorRole, "ws3": api.EditorRole, "ws4": api.ViewerRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test2",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws1": api.EditorRole, "ws2": api.EditorRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test3",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws1": api.ViewerRole, "ws2": api.EditorRole, "ws4": api.ViewerRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test4",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws4": api.AdminRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test5",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws2": api.AdminRole, "ws3": api.EditorRole, "ws4": api.ViewerRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+	{
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+		Email:         "user1@test.com",
+		EmailVerified: true,
+		FamilyName:    "testFamily",
+		GivenName:     "testName",
+		Locale:        "locale test",
+		Name:          "testName",
+		Nickname:      "testNick",
+		Picture:       "testURL",
+		UserId:        "test6",
+		UserMetadata: auth0.Metadata{
+			WorkspaceAccess: map[string]api.Role{"ws1": api.ViewerRole, "ws2": api.ViewerRole, "ws3": api.ViewerRole, "ws4": api.ViewerRole},
+		},
+		LastLogin:     time.Now(),
+		LastIp:        "testIP",
+		LoginsCount:   1,
+		AppMetadata:   auth0.Metadata{},
+		Username:      "testUserName",
+		PhoneNumber:   "testPhone",
+		PhoneVerified: true,
+		Multifactor:   []string{"testMultifactior"},
+		Blocked:       false,
+	},
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	fmt.Fprint(w, fmt.Sprintf("Welcome to the homepage! %s", id))
+}
+
+func mockDeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/api/v2/users/")
+	fmt.Println("the ID is : ", id)
+	w.WriteHeader(http.StatusOK)
+}
+
+func mockFillTocken(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	res := tokenStruct{
+		AccessToken: "test",
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
+func mockGetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := strings.TrimPrefix(r.URL.Path, "/api/v2/users/")
+
+	for _, user := range testUsers {
+		if user.UserId == id {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func mockGetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(testUsers)
+}
