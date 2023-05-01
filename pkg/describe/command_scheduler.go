ebaf0840472060e92b799e652ea18fe651be3a0b
Mahan Zendedel <mahan.zendedel@keibi.io>
2023-05-01 14:20:47 +0300

    feat: add azure lambda handling

--- pkg/describe/command_scheduler.go
+++ pkg/describe/command_scheduler.go
@@ -60,7 +60,7 @@ var (
 	OnboardBaseURL             = os.Getenv("ONBOARD_BASE_URL")
 	AuthGRPCURI                = os.Getenv("AUTH_GRPC_URI")
 
-	LambdaFuncURL           = os.Getenv("LAMBDA_FUNC_URL")
+	LambdaFuncsBaseURL      = os.Getenv("LAMBDA_FUNCS_BASE_URL")
 	KeyARN                  = os.Getenv("KMS_KEY_ARN")
 	KeyRegion               = os.Getenv("KMS_KEY_REGION")
 	DescribeDeliverEndpoint = os.Getenv("DESCRIBE_DELIVER_ENDPOINT")
