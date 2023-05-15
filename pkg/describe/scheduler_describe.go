15b5300d0e377069ebe12cd7fc6cfebcb5cb179a
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-05-15 15:09:56 +0300

    fix: Adds more concurrency on calling lambda functions

--- pkg/describe/scheduler_describe.go
+++ pkg/describe/scheduler_describe.go
@@ -25,7 +25,7 @@ const (
 	MaxTriggerPerMinute           = 10000
 	MaxTriggerPerAccountPerMinute = 60
 	MaxQueued                     = 10000
-	MaxConcurrentCall             = 100
+	MaxConcurrentCall             = 1000
 )
 
 type CloudNativeCall struct {
