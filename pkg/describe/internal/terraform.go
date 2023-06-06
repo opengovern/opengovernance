fbab7e0c9338743416003b82a76c5ec5af6bf123
artaasadi <arta-c@kaytu.io>
2023-06-06 14:15:26 +0330

    azure parse account added

--- pkg/describe/internal/terraform.go
+++ pkg/describe/internal/terraform.go
@@ -22,11 +22,21 @@ func getTypes(content string) ([]string, error) {
 func ParseAccountsFromArns(arns []string) ([]string, error) {
 	haveAcc := make(map[string]bool)
 	var accounts []string
-	for _, arn := range arns {
-		splitArn := strings.Split(arn, ":")
-		if _, value := haveAcc[splitArn[4]]; !value && splitArn[4] != "" {
-			haveAcc[splitArn[4]] = true
-			accounts = append(accounts, splitArn[4])
+	if arns[0] == "/" { // Azure
+		for _, arn := range arns {
+			splitArn := strings.Split(arn, "/")
+			if _, value := haveAcc[splitArn[2]]; !value && splitArn[2] != "" {
+				haveAcc[splitArn[2]] = true
+				accounts = append(accounts, splitArn[2])
+			}
+		}
+	} else { // AWS
+		for _, arn := range arns {
+			splitArn := strings.Split(arn, ":")
+			if _, value := haveAcc[splitArn[4]]; !value && splitArn[4] != "" {
+				haveAcc[splitArn[4]] = true
+				accounts = append(accounts, splitArn[4])
+			}
 		}
 	}
 	return accounts, nil
