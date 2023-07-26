dcb84a2ab9212fbb62f7eb7ad27e58f68ffca121
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-07-26 20:10:52 +0400

    feat: Adds analytics region data

--- pkg/analytics/es/region.go
+++ pkg/analytics/es/region.go
@@ -1,12 +1,16 @@
 package es
 
 import (
+	"github.com/google/uuid"
 	"github.com/kaytu-io/kaytu-engine/pkg/summarizer/es"
+	"github.com/kaytu-io/kaytu-util/pkg/source"
 	"strconv"
 )
 
 type RegionMetricTrendSummary struct {
 	Region        string                `json:"region"`
+	ConnectionID  uuid.UUID             `json:"connection_id"`
+	Connector     source.Type           `json:"connector"`
 	EvaluatedAt   int64                 `json:"evaluated_at"`
 	MetricID      string                `json:"metric_id"`
 	ResourceCount int                   `json:"resource_count"`
@@ -16,6 +20,7 @@ type RegionMetricTrendSummary struct {
 func (r RegionMetricTrendSummary) KeysAndIndex() ([]string, string) {
 	keys := []string{
 		r.Region,
+		r.ConnectionID.String(),
 		r.MetricID,
 		strconv.FormatInt(r.EvaluatedAt, 10),
 		string(es.MetricTrendRegionSummary),
