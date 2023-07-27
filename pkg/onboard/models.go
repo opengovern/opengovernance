420783489ba37cf0caff9db2e596e35d545f4818
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-07-27 18:47:16 +0400

    feat: add get tags for azure subs

--- pkg/onboard/models.go
+++ pkg/onboard/models.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	"github.com/aws/aws-sdk-go-v2/service/organizations/types"
@@ -220,14 +219,26 @@ func NewAWSSource(logger *zap.Logger, cfg describe.AWSAccountConfig, account aws
 type AzureConnectionMetadata struct {
 	SubscriptionID string                       `json:"subscription_id"`
 	SubModel       armsubscription.Subscription `json:"subscription_model"`
-	SubTags        []armresources.TagDetails    `json:"subscription_tags"`
+	SubTags        map[string][]string          `json:"subscription_tags"`
 }
 
 func NewAzureConnectionMetadata(sub azureSubscription) AzureConnectionMetadata {
 	metadata := AzureConnectionMetadata{
 		SubscriptionID: sub.SubscriptionID,
 		SubModel:       sub.SubModel,
-		SubTags:        sub.SubTags,
+		SubTags:        make(map[string][]string),
+	}
+	for _, tag := range sub.SubTags {
+		if tag.TagName == nil || tag.Count == nil {
+			continue
+		}
+		metadata.SubTags[*tag.TagName] = make([]string, 0, len(tag.Values))
+		for _, value := range tag.Values {
+			if value == nil || value.TagValue == nil {
+				continue
+			}
+			metadata.SubTags[*tag.TagName] = append(metadata.SubTags[*tag.TagName], *value.TagValue)
+		}
 	}
 
 	return metadata
