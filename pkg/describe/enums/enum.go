832d2b309150078248cb183543241caa393f0ca0
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-08-28 00:05:51 +0400

    feat: Adds cost full discovery

--- pkg/describe/enums/enum.go
+++ pkg/describe/enums/enum.go
@@ -3,8 +3,9 @@ package enums
 type DescribeTriggerType string
 
 const (
-	DescribeTriggerTypeInitialDiscovery DescribeTriggerType = "initial_discovery"
-	DescribeTriggerTypeScheduled        DescribeTriggerType = "scheduled" // default
-	DescribeTriggerTypeManual           DescribeTriggerType = "manual"
-	DescribeTriggerTypeStack            DescribeTriggerType = "stack"
+	DescribeTriggerTypeInitialDiscovery  DescribeTriggerType = "initial_discovery"
+	DescribeTriggerTypeCostFullDiscovery DescribeTriggerType = "cost_full_discovery" // default
+	DescribeTriggerTypeScheduled         DescribeTriggerType = "scheduled"           // default
+	DescribeTriggerTypeManual            DescribeTriggerType = "manual"
+	DescribeTriggerTypeStack             DescribeTriggerType = "stack"
 )
