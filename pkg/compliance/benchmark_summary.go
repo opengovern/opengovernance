8199b496f86dd35bf213ee7b9233e2733df73f5e
saber khazaei <saberk@kaytu.io>
2023-08-28 19:01:59 +0400

    fix: add span smaller for compliance

--- pkg/compliance/benchmark_summary.go
+++ pkg/compliance/benchmark_summary.go
@@ -1,15 +1,19 @@
 package compliance
 
-func (h *HttpHandler) GetBenchmarkTreeIDs(rootID string) ([]string, error) {
+import "context"
+
+func (h *HttpHandler) GetBenchmarkTreeIDs(ctx context.Context, rootID string) ([]string, error) {
 	ids := []string{rootID}
+	_, spanGB := tracer.Start(ctx, "GetBenchmark")
 
 	root, err := h.db.GetBenchmark(rootID)
 	if err != nil {
 		return nil, err
 	}
+	spanGB.End()
 
 	for _, child := range root.Children {
-		cids, err := h.GetBenchmarkTreeIDs(child.ID)
+		cids, err := h.GetBenchmarkTreeIDs(ctx, child.ID)
 		if err != nil {
 			return nil, err
 		}
