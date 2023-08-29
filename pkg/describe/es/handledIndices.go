edd32c39eec9d583e1d00a344b6489bc10e6be67
artaasadi <arta-c@kaytu.io>
2023-08-29 09:22:57 +0330

    fix: add handledIndices

---
+++ pkg/describe/es/handledIndices.go
@@ -0,0 +1,25 @@
+package es
+
+var awsHandledResourceTypes = []string{"AWS::CertificateManager::Certificate", "AWS::Athena::QueryExecution",
+	"AWS::IAM::Policy", "AWS::ECR::Repository", "AWS::ECS::Service", "AWS::CloudFormation::Stack"}
+
+var azureHandledResourceTypes = []string{"Microsoft.Network/networkSecurityGroups", "Microsoft.Web/sites",
+	"Microsoft.Network/virtualNetworks/subnets"}
+
+func IsHandledAWSResourceType(resourceType string) bool {
+	for _, r := range awsHandledResourceTypes {
+		if resourceType == r {
+			return true
+		}
+	}
+	return false
+}
+
+func IsHandledAzureResourceType(resourceType string) bool {
+	for _, r := range azureHandledResourceTypes {
+		if resourceType == r {
+			return true
+		}
+	}
+	return false
+}
