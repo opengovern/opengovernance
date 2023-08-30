7ec1242759a076629357ba00cdecb6c8d59a253e
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-08-30 20:11:51 +0400

    fix: Fixes logs

--- pkg/inventory/http_routes.go
+++ pkg/inventory/http_routes.go
@@ -494,7 +494,9 @@ func (h *HttpHandler) MigrateSpendPart(summarizerJobID int, isAWS bool) error {
 				PeriodEnd:   hit.PeriodEnd * 1000,
 			}
 			key = fmt.Sprintf("%s-%s-%s", connector.Connector, metricID, dateStr)
-			fmt.Println(key, connector.CostValue)
+			if dateStr == "2023-07-05" {
+				fmt.Println(key, connector.CostValue)
+			}
 			if v, ok := connectorMap[key]; ok {
 				v.CostValue += connector.CostValue
 				connectorMap[key] = v
