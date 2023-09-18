e25b3eeb448f38457f7ad7f4935914a175fdc885
artaasadi <arta-c@kaytu.io>
2023-09-18 17:21:04 +0330

    fix: big resources

--- pkg/describe/es/handled_indices.go
+++ pkg/describe/es/handled_indices.go
@@ -2,7 +2,8 @@ package es
 
 var awsHandledResourceTypes = []string{"AWS::CertificateManager::Certificate", "AWS::Athena::QueryExecution",
 	"AWS::IAM::Policy", "AWS::ECR::Repository", "AWS::ECS::Service", "AWS::CloudFormation::Stack", "AWS::EC2::Instance",
-	"AWS::AccessAnalyzer::Analyzer", "AWS::Glue::CatalogTable", "AWS::RDS::DBClusterParameterGroup", "AWS::WAFv2::IPSet"}
+	"AWS::AccessAnalyzer::Analyzer", "AWS::Glue::CatalogTable", "AWS::RDS::DBClusterParameterGroup", "AWS::WAFv2::IPSet",
+	"AWS::EC2::RouteTable", "AWS::WAFv2::RuleGroup", "AWS::WAFv2::WebACL"}
 
 var azureHandledResourceTypes = []string{"Microsoft.Network/networkSecurityGroups", "Microsoft.Web/sites",
 	"Microsoft.Network/virtualNetworks/subnets", "Microsoft.Network/frontDoors", "Microsoft.Network/loadBalancers",
@@ -10,7 +11,7 @@ var azureHandledResourceTypes = []string{"Microsoft.Network/networkSecurityGroup
 	"Microsoft.Network/applicationGateways", "Microsoft.LoadBalancer/backendAddressPools", "Microsoft.KeyVault/vaults",
 	"Microsoft.DataFactory/factoriesDatasets", "Microsoft.Authorization/roleDefinitions", "Microsoft.Logic/workflows",
 	"Microsoft.Compute/virtualMachines", "Microsoft.DBforMySQL/servers", "Microsoft.DBforPostgreSQL/servers",
-	"Microsoft.Compute/virtualMachineScaleSets", "Microsoft.Sql/servers"}
+	"Microsoft.Compute/virtualMachineScaleSets", "Microsoft.Sql/servers", "Microsoft.Web/plan"}
 
 func IsHandledAWSResourceType(resourceType string) bool {
 	for _, r := range awsHandledResourceTypes {
