fe66c0cfdd6882ff0e15558372a5c809eb2e45f7
artaasadi <arta-c@kaytu.io>
2023-09-21 11:11:59 +0330

    fix: get failed describe jobs

--- pkg/describe/es/handled_indices.go
+++ pkg/describe/es/handled_indices.go
@@ -3,7 +3,7 @@ package es
 var awsHandledResourceTypes = []string{"AWS::CertificateManager::Certificate", "AWS::Athena::QueryExecution",
 	"AWS::IAM::Policy", "AWS::ECR::Repository", "AWS::ECS::Service", "AWS::CloudFormation::Stack", "AWS::EC2::Instance",
 	"AWS::AccessAnalyzer::Analyzer", "AWS::Glue::CatalogTable", "AWS::RDS::DBClusterParameterGroup", "AWS::WAFv2::IPSet",
-	"AWS::EC2::RouteTable", "AWS::WAFv2::RuleGroup", "AWS::WAFv2::WebACL"}
+	"AWS::EC2::RouteTable", "AWS::WAFv2::RuleGroup", "AWS::WAFv2::WebACL", "AWS::S3::Bucket"}
 
 var azureHandledResourceTypes = []string{"Microsoft.Network/networkSecurityGroups", "Microsoft.Web/sites",
 	"Microsoft.Network/virtualNetworks/subnets", "Microsoft.Network/frontDoors", "Microsoft.Network/loadBalancers",
