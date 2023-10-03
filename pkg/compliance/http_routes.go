73ec57acc1d74e2b16cbfe2466d9664e2568784a
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-10-03 22:32:11 +0400

    fix: Fixes complie error

--- pkg/compliance/http_routes.go
+++ pkg/compliance/http_routes.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/kaytu-io/kaytu-azure-describer/pkg/kaytu-es-sdk"
+	"github.com/kaytu-io/kaytu-util/pkg/kaytu-es-sdk"
 	"github.com/labstack/echo/v4"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
