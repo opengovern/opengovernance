8ac8ae724784f2b22244997ff8fe36e4a6cf08df
artaasadi <arta-c@kaytu.io>
2023-10-04 19:46:17 +0330

    fix: comment bug

--- pkg/describe/db.go
+++ pkg/describe/db.go
@@ -166,7 +166,7 @@ FROM
 WHERE
 	status = ? AND
 	created_at > now() - interval '3 day' AND 
-	NOT(error_code IN ('JobTimeOut', 'InvalidApiVersionParameter', 'AuthorizationFailed', 'AccessDeniedException', 'InvalidAuthenticationToken', 'AccessDenied', 'InsufficientPrivilegesException', '403', '404', '401', '400')) AND
+	NOT(error_code IN ('InvalidApiVersionParameter', 'AuthorizationFailed', 'AccessDeniedException', 'InvalidAuthenticationToken', 'AccessDenied', 'InsufficientPrivilegesException', '403', '404', '401', '400')) AND
 	(retry_count < 5 OR retry_count IS NULL)
 	ORDER BY id DESC LIMIT ?
 `, api.DescribeResourceJobFailed, limit).Find(&job)
