29c113bd531edcc2d63ce5776ad0e5e76cc24858
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-10-06 19:48:28 +0400

    feat: add resource collection scheduler for insights

--- pkg/describe/models.go
+++ pkg/describe/models.go
@@ -96,13 +96,14 @@ type DescribeConnectionJob struct {
 
 type InsightJob struct {
 	gorm.Model
-	InsightID      uint   `gorm:"index:idx_source_id_insight_id"`
-	SourceID       string `gorm:"index:idx_source_id_insight_id"`
-	AccountID      string
-	SourceType     source.Type
-	Status         insightapi.InsightJobStatus
-	FailureMessage string
-	IsStack        bool
+	InsightID          uint   `gorm:"index:idx_source_id_insight_id"`
+	SourceID           string `gorm:"index:idx_source_id_insight_id"`
+	AccountID          string
+	SourceType         source.Type
+	Status             insightapi.InsightJobStatus
+	FailureMessage     string
+	IsStack            bool
+	ResourceCollection *string
 }
 
 type SummarizerJob struct {
