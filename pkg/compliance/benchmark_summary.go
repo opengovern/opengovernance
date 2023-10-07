eb6b2ed6c16ae7713ccf04366eecbf5c77be744e
artaasadi <arta-c@kaytu.io>
2023-10-07 23:19:10 +0330

    fix: benchmark tree ids

--- pkg/compliance/benchmark_summary.go
+++ pkg/compliance/benchmark_summary.go
@@ -2,26 +2,27 @@ package compliance
 
 import (
 	"context"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
+	"fmt"
 )
 
 func (h *HttpHandler) GetBenchmarkTreeIDs(ctx context.Context, rootID string) ([]string, error) {
 	ids := []string{rootID}
 	// tracer :
-	_, span2 := tracer.Start(ctx, "new_GetBenchmark", trace.WithSpanKind(trace.SpanKindServer))
-	span2.SetName("new_GetBenchmark")
-
+	//_, span2 := tracer.Start(ctx, "new_GetBenchmark", trace.WithSpanKind(trace.SpanKindServer))
+	//span2.SetName("new_GetBenchmark")
+	h.logger.Info(fmt.Sprintf("RootId: %s", rootID))
 	root, err := h.db.GetBenchmark(rootID)
 	if err != nil {
 		return nil, err
 	}
-	span2.AddEvent("information", trace.WithAttributes(
-		attribute.String("benchmark ID", root.ID),
-	))
-	span2.End()
+	h.logger.Info(fmt.Sprintf("Root: %v", root))
+	//span2.AddEvent("information", trace.WithAttributes(
+	//	attribute.String("benchmark ID", root.ID),
+	//))
+	//span2.End()
 
 	for _, child := range root.Children {
+		h.logger.Info(fmt.Sprintf("Child: %v", child))
 		cids, err := h.GetBenchmarkTreeIDs(ctx, child.ID)
 		if err != nil {
 			return nil, err
