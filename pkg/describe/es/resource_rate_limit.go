ea8a7079254c7f5ee7e01dabf1cb833b42acc10f
artaasadi <arta-c@kaytu.io>
2023-10-12 13:12:28 +0330

    fix: update dependencies

--- pkg/describe/es/resource_rate_limit.go
+++ pkg/describe/es/resource_rate_limit.go
@@ -3,6 +3,7 @@ package es
 var ResourceRateLimit = map[string]int{
 	"Microsoft.Management/groups":                 1,
 	"Microsoft.CostManagement/CostByResourceType": 1,
+	"Microsoft.Storage/tables":                    1,
 	"AWS::Organizations::Account":                 1,
 	"AWS::Shield::ProtectionGroup":                1,
 	"AWS::IAM::Policy":                            1,
