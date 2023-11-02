2a0498af33e4dddeb06f0a8efa5ada2ea8ca425f
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-11-03 00:19:03 +0400

    fix: Fixes job

--- pkg/describe/schedulers/compliance/publisher.go
+++ pkg/describe/schedulers/compliance/publisher.go
@@ -4,17 +4,45 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/kaytu-io/kaytu-engine/pkg/auth/api"
 	"github.com/kaytu-io/kaytu-engine/pkg/compliance/runner"
+	"github.com/kaytu-io/kaytu-engine/pkg/internal/httpclient"
 	kafka2 "github.com/kaytu-io/kaytu-util/pkg/kafka"
+	"github.com/kaytu-io/kaytu-util/pkg/source"
 )
 
 func (s *JobScheduler) runPublisher() error {
+	ctx := &httpclient.Context{UserRole: api.InternalRole}
+
 	runners, err := s.db.FetchCreatedRunners()
 	if err != nil {
 		return err
 	}
 
-	for _, job := range runners {
+	for _, it := range runners {
+		query, err := s.complianceClient.GetQuery(ctx, it.QueryID)
+		if err != nil {
+			return err
+		}
+
+		callers, err := it.GetCallers()
+		if err != nil {
+			return err
+		}
+
+		job := runner.Job{
+			ID:        it.ID,
+			CreatedAt: it.CreatedAt,
+			ExecutionPlan: runner.ExecutionPlan{
+				Callers:              callers,
+				QueryID:              it.QueryID,
+				QueryEngine:          query.Engine,
+				QueryConnector:       source.Type(query.Connector),
+				ConnectionID:         it.ConnectionID,
+				ResourceCollectionID: it.ResourceCollectionID,
+			},
+		}
+
 		jobJson, err := json.Marshal(job)
 		if err != nil {
 			_ = s.db.UpdateRunnerJob(job.ID, runner.ComplianceRunnerFailed, err.Error())
