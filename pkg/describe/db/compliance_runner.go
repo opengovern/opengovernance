9550a0aaded603bf9febe88babadd49cc5322821
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-11-02 23:35:38 +0400

    fix: Fixes db

--- pkg/describe/db/compliance_runner.go
+++ pkg/describe/db/compliance_runner.go
@@ -32,7 +32,7 @@ func (db Database) FetchCreatedRunners() ([]model.ComplianceRunner, error) {
 }
 
 func (db Database) RetryFailedRunners() error {
-	tx := db.ORM.Exec("UPDATE compliance_runner SET retry_count = retry_count + 1, status = 'CREATED' WHERE status = 'FAILED' AND retry_count < 3 AND updated_at < NOW() - interval '5 minutes'")
+	tx := db.ORM.Exec("UPDATE compliance_runners SET retry_count = retry_count + 1, status = 'CREATED' WHERE status = 'FAILED' AND retry_count < 3 AND updated_at < NOW() - interval '5 minutes'")
 	if tx.Error != nil {
 		return tx.Error
 	}
