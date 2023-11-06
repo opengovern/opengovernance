9fca231d8d1e3675557322b8d231f37f17eecdbb
artaasadi <arta-c@kaytu.io>
2023-11-06 17:46:04 +0100

    feat: add ec2 volume calculation

--- pkg/cost-estimator/resource_types.go
+++ pkg/cost-estimator/resource_types.go
@@ -1,6 +1,6 @@
 package cost_estimator
 
-type CostEstimatorFunc func(h *HttpHandler, resourceId string, timeInterval int) (float64, error)
+type CostEstimatorFunc func(h *HttpHandler, resourceId string) (float64, error)
 
 var azureResourceTypes = map[string]CostEstimatorFunc{
 	"Microsoft.Compute/virtualMachines": GetComputeVirtualMachineCost,
@@ -9,4 +9,5 @@ var azureResourceTypes = map[string]CostEstimatorFunc{
 
 var awsResourceTypes = map[string]CostEstimatorFunc{
 	"AWS::EC2::Instance": GetEC2InstanceCost,
+	"AWS::EC2::Volume":   GetEC2VolumeCost,
 }
--- pkg/workspace/server.go
+++ pkg/workspace/server.go
@@ -169,7 +169,8 @@ func (s *Server) Register(e *echo.Echo) {
 	organizationGroup.DELETE("/:organizationId", httpserver2.AuthorizeHandler(s.DeleteOrganization, authapi.EditorRole))
 
 	costEstimatorGroup := v1Group.Group("/cost_estimator")
-	costEstimatorGroup.GET("/ec2_instance/:interval", httpserver2.AuthorizeHandler(s.GetEC2InstancePrice, authapi.InternalRole))
+	costEstimatorGroup.GET("/ec2instance", httpserver2.AuthorizeHandler(s.GetEC2InstancePrice, authapi.InternalRole))
+	costEstimatorGroup.GET("/ec2volume", httpserver2.AuthorizeHandler(s.GetEC2VolumePrice, authapi.InternalRole))
 }
 
 func (s *Server) Start() error {
