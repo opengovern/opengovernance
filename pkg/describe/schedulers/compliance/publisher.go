fde1ecf2d4dbdf9d5d376b747976f417cfdaf688
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-11-07 18:04:50 +0400

    fix: retry bug

--- pkg/describe/schedulers/compliance/publisher.go
+++ pkg/describe/schedulers/compliance/publisher.go
@@ -9,6 +9,7 @@ import (
 	"github.com/kaytu-io/kaytu-engine/pkg/internal/httpclient"
 	kafka2 "github.com/kaytu-io/kaytu-util/pkg/kafka"
 	"github.com/kaytu-io/kaytu-util/pkg/source"
+	"go.uber.org/zap"
 )
 
 func (s *JobScheduler) runPublisher() error {
@@ -18,22 +19,25 @@ func (s *JobScheduler) runPublisher() error {
 	for i := 0; i < 10; i++ {
 		runners, err := s.db.FetchCreatedRunners()
 		if err != nil {
-			return err
+			s.logger.Error("failed to fetch created runners", zap.Error(err))
+			continue
 		}
 
 		if len(runners) == 0 {
-			return nil
+			break
 		}
 
 		for _, it := range runners {
 			query, err := s.complianceClient.GetQuery(ctx, it.QueryID)
 			if err != nil {
-				return err
+				s.logger.Error("failed to get query", zap.Error(err), zap.String("queryId", it.QueryID), zap.Uint("runnerId", it.ID))
+				continue
 			}
 
 			callers, err := it.GetCallers()
 			if err != nil {
-				return err
+				s.logger.Error("failed to get callers", zap.Error(err), zap.Uint("runnerId", it.ID))
+				continue
 			}
 
 			job := runner.Job{
@@ -52,14 +56,16 @@ func (s *JobScheduler) runPublisher() error {
 			jobJson, err := json.Marshal(job)
 			if err != nil {
 				_ = s.db.UpdateRunnerJob(job.ID, runner.ComplianceRunnerFailed, job.CreatedAt, nil, err.Error())
-				return err
+				s.logger.Error("failed to marshal job", zap.Error(err), zap.Uint("runnerId", it.ID))
+				continue
 			}
 
 			msg := kafka2.Msg(fmt.Sprintf("job-%d", job.ID), jobJson, "", runner.JobQueue, kafka.PartitionAny)
 			_, err = kafka2.SyncSend(s.logger, s.kafkaProducer, []*kafka.Message{msg}, nil)
 			if err != nil {
 				_ = s.db.UpdateRunnerJob(job.ID, runner.ComplianceRunnerFailed, job.CreatedAt, nil, err.Error())
-				return err
+				s.logger.Error("failed to send job", zap.Error(err), zap.Uint("runnerId", it.ID))
+				continue
 			}
 
 			_ = s.db.UpdateRunnerJob(job.ID, runner.ComplianceRunnerInProgress, job.CreatedAt, nil, "")
@@ -68,6 +74,7 @@ func (s *JobScheduler) runPublisher() error {
 
 	err := s.db.RetryFailedRunners()
 	if err != nil {
+		s.logger.Error("failed to retry failed runners", zap.Error(err))
 		return err
 	}
 
