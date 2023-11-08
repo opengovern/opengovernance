aded60b0a20e6559997e70cc79b96a037555a286
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-11-08 19:43:17 +0400

    fix: total finding count

--- pkg/compliance/runner/job.go
+++ pkg/compliance/runner/job.go
@@ -129,13 +129,17 @@ func (w *Worker) RunJob(j Job) (int, error) {
 		zap.Int("res_count", len(res.Data)),
 		zap.Int("caller_count", len(j.ExecutionPlan.Callers)),
 	)
-	totalFindingCount := 0
+	totalFindingCountMap := make(map[string]int)
 	for _, caller := range j.ExecutionPlan.Callers {
 		findings, err := j.ExtractFindings(w.logger, caller, res, *query)
 		if err != nil {
 			return 0, err
 		}
-		totalFindingCount += len(findings)
+
+		mapKey := fmt.Sprintf("%s---___---%s", caller.RootBenchmark, caller.PolicyID)
+		if _, ok := totalFindingCountMap[mapKey]; !ok {
+			totalFindingCountMap[mapKey] = len(findings)
+		}
 
 		var docs []kafka.Doc
 		for _, f := range findings {
@@ -154,6 +158,12 @@ func (w *Worker) RunJob(j Job) (int, error) {
 			return 0, err
 		}
 	}
+
+	totalFindingCount := 0
+	for _, v := range totalFindingCountMap {
+		totalFindingCount += v
+	}
+
 	w.logger.Info("Finished job",
 		zap.Uint("job_id", j.ID),
 		zap.String("query_id", j.ExecutionPlan.QueryID),
