e47a7ac58935b5c9c0f7417e6b7834f63f1abb11
Mahan Zendedel DH <mahanmmi@gmail.com>
2023-11-13 14:50:21 +0400

    fix: add logs

--- pkg/describe/schedulers/compliance/summarizer.go
+++ pkg/describe/schedulers/compliance/summarizer.go
@@ -81,8 +81,11 @@ func (s *JobScheduler) getSankDocumentCountBenchmark(benchmarkId string, parentJ
 }
 
 func (s *JobScheduler) runSummarizer() error {
+	s.logger.Info("checking for benchmarks to summarize")
+
 	err := s.db.SetJobToRunnersInProgress()
 	if err != nil {
+		s.logger.Error("failed to set jobs to runners in progress", zap.Error(err))
 		return err
 	}
 
