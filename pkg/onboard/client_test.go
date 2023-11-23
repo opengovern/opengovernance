d729686cd1b7604d7b9fa3049874ceecbb504af1
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-11-23 13:18:18 +0400

    fix: Fixes connection healthcheck

--- pkg/onboard/client_test.go
+++ pkg/onboard/client_test.go
@@ -1,11 +1,15 @@
 package onboard
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
-	apiv2 "github.com/kaytu-io/kaytu-engine/pkg/onboard/api/v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/google/uuid"
 	"github.com/kaytu-io/kaytu-util/pkg/source"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestName(t *testing.T) {
@@ -13,15 +17,24 @@ func TestName(t *testing.T) {
 		masterAccessKey: "AKIAXMSWAQYFWO7WVLCK",
 		masterSecretKey: "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",
 	}
-	resp, err := h.createAWSCredential(apiv2.CreateCredentialV2Request{
-		Connector: source.CloudAWS,
-		Config: apiv2.AWSCredentialV2Config{
-			AssumeRoleName:      "KaytuOrganizationCrossAccountRole",
-			AccountID:           "517592840862",
-			HealthCheckPolicies: nil,
-			ExternalId:          nil,
-		},
-	})
+	cred := Credential{
+		ID:                  uuid.UUID{},
+		Name:                aws.String("o-y3x84b8zm6"),
+		ConnectorType:       source.CloudAWS,
+		Secret:              "AQICAHhNzpIghOTsfDBkWLoHl+9ZRYRT0FG2G6MV4j1Yu7S1cQElu7EyHHDE2EFdq95EHk/1AAAA+TCB9gYJKoZIhvcNAQcGoIHoMIHlAgEAMIHfBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDMzzYzCC+85R0R99ygIBEICBse/Yj2KiAE+D55136/u+brTmJBO48LGwZDdjfAvLXCUBmlTwC23YYL5C92bAAq50VACg4Mt2Lfi2T+yGaF3zQfBhMUFrvXVWa1eVAgxUsMeymlaewKSqu8vegPZAJaGlBCSOtI4uBch0KryFibyz7RrWt2Z6xxLjgvGX33SG9kOV4ZYMjBrJ56Io1LWLb8OwL+nZt6UHwdglqIOErRtbwaE9yalxzOYOCkbQ+FrMmvw6yg==",
+		CredentialType:      "manual-aws-org",
+		Enabled:             true,
+		AutoOnboardEnabled:  true,
+		LastHealthCheckTime: time.Time{},
+		HealthStatus:        source.HealthStatusHealthy,
+		HealthReason:        nil,
+		Metadata:            nil,
+		CreatedAt:           time.Time{},
+		UpdatedAt:           time.Time{},
+		DeletedAt:           sql.NullTime{},
+		Version:             2,
+	}
+	onboardedSources, err := h.autoOnboardAWSAccountsV2(context.Background(), cred, 50)
 	assert.NoError(t, err)
-	fmt.Println(resp)
+	fmt.Println(onboardedSources)
 }
--- pkg/onboard/http_routes.go
+++ pkg/onboard/http_routes.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/kaytu-io/kaytu-engine/pkg/demo"
 	apiv2 "github.com/kaytu-io/kaytu-engine/pkg/onboard/api/v2"
 	"go.opentelemetry.io/otel"
@@ -1817,47 +1818,98 @@ func (h HttpHandler) checkConnectionHealth(ctx context.Context, connection Sourc
 	var isAttached bool
 	switch connection.Type {
 	case source.CloudAWS:
-		var awsCnf describe.AWSAccountConfig
-		awsCnf, err = describe.AWSAccountConfigFromMap(cnf)
-		if err != nil {
-			h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
-			return connection, err
-		}
-		assumeRoleArn := kaytuAws.GetRoleArnFromName(connection.SourceId, awsCnf.AssumeRoleName)
-		var sdkCnf aws.Config
-		if awsCnf.AccountID != connection.SourceId {
-			sdkCnf, err = kaytuAws.GetConfig(ctx, awsCnf.AccessKey, awsCnf.SecretKey, "", assumeRoleArn, awsCnf.ExternalID)
-		} else {
-			sdkCnf, err = kaytuAws.GetConfig(ctx, awsCnf.AccessKey, awsCnf.SecretKey, "", "", nil)
-		}
-		if err != nil {
-			h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
-			return connection, err
-		}
-		if awsCnf.AccountID != connection.SourceId {
-			isAttached, err = kaytuAws.CheckAttachedPolicy(h.logger, sdkCnf, awsCnf.AssumeRoleName, kaytuAws.GetPolicyArnFromName(connection.SourceId, awsCnf.AssumeRolePolicyName))
-		} else {
-			isAttached, err = kaytuAws.CheckAttachedPolicy(h.logger, sdkCnf, "", kaytuAws.GetPolicyArnFromName(connection.SourceId, awsCnf.AssumeRolePolicyName))
-		}
-		if err == nil && isAttached && updateMetadata {
-			if sdkCnf.Region == "" {
-				sdkCnf.Region = "us-east-1"
-			}
-			var awsAccount *awsAccount
-			awsAccount, err = currentAwsAccount(ctx, h.logger, sdkCnf)
+		if connection.Credential.Version == 2 {
+			awsCnf, err := apiv2.AWSCredentialV2ConfigFromMap(cnf)
 			if err != nil {
-				h.logger.Error("failed to get current aws account", zap.Error(err), zap.String("sourceId", connection.SourceId))
+				h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
 				return connection, err
 			}
-			metadata, err2 := NewAWSConnectionMetadata(h.logger, awsCnf, connection, *awsAccount)
-			if err2 != nil {
-				h.logger.Error("failed to get aws connection metadata", zap.Error(err2), zap.String("sourceId", connection.SourceId))
-			}
-			jsonMetadata, err2 := json.Marshal(metadata)
-			if err2 != nil {
+			assumeRoleArn := kaytuAws.GetRoleArnFromName(connection.SourceId, awsCnf.AssumeRoleName)
+			sdkCnf, err := kaytuAws.GetConfig(ctx, h.masterAccessKey, h.masterSecretKey, "", assumeRoleArn, awsCnf.ExternalId)
+			if err != nil {
+				h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
 				return connection, err
 			}
-			connection.Metadata = jsonMetadata
+
+			iamClient := iam.NewFromConfig(sdkCnf)
+			paginator := iam.NewListAttachedRolePoliciesPaginator(iamClient, &iam.ListAttachedRolePoliciesInput{
+				RoleName: &awsCnf.AssumeRoleName,
+			})
+			var policyNames []string
+			for paginator.HasMorePages() {
+				page, err := paginator.NextPage(ctx)
+				if err != nil {
+					return connection, err
+				}
+				for _, policy := range page.AttachedPolicies {
+					policyNames = append(policyNames, *policy.PolicyName)
+				}
+			}
+
+			isAttached = true
+			if len(awsCnf.HealthCheckPolicies) == 0 {
+				awsCnf.HealthCheckPolicies = []string{
+					"AWSAccountActivityAccess",
+					"AWSBillingReadOnlyAccess",
+					"AWSBudgetsReadOnlyAccess",
+					"AWSOrganizationsReadOnlyAccess",
+					"KaytuAdditionalResourceReadOnly",
+					"ReadOnlyAccess",
+					"SecurityAudit",
+				}
+			}
+			for _, policyName := range awsCnf.HealthCheckPolicies {
+				if !utils.Includes(policyNames, policyName) {
+					h.logger.Error("policy is not there", zap.String("policyName", policyName), zap.Strings("attachedPolicies", policyNames))
+					isAttached = false
+				}
+			}
+
+			//TODO
+
+		} else {
+			var awsCnf describe.AWSAccountConfig
+			awsCnf, err = describe.AWSAccountConfigFromMap(cnf)
+			if err != nil {
+				h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
+				return connection, err
+			}
+			assumeRoleArn := kaytuAws.GetRoleArnFromName(connection.SourceId, awsCnf.AssumeRoleName)
+			var sdkCnf aws.Config
+			if awsCnf.AccountID != connection.SourceId {
+				sdkCnf, err = kaytuAws.GetConfig(ctx, awsCnf.AccessKey, awsCnf.SecretKey, "", assumeRoleArn, awsCnf.ExternalID)
+			} else {
+				sdkCnf, err = kaytuAws.GetConfig(ctx, awsCnf.AccessKey, awsCnf.SecretKey, "", "", nil)
+			}
+			if err != nil {
+				h.logger.Error("failed to get aws config", zap.Error(err), zap.String("sourceId", connection.SourceId))
+				return connection, err
+			}
+			if awsCnf.AccountID != connection.SourceId {
+				isAttached, err = kaytuAws.CheckAttachedPolicy(h.logger, sdkCnf, awsCnf.AssumeRoleName, kaytuAws.GetPolicyArnFromName(connection.SourceId, awsCnf.AssumeRolePolicyName))
+			} else {
+				isAttached, err = kaytuAws.CheckAttachedPolicy(h.logger, sdkCnf, "", kaytuAws.GetPolicyArnFromName(connection.SourceId, awsCnf.AssumeRolePolicyName))
+			}
+			if err == nil && isAttached && updateMetadata {
+				if sdkCnf.Region == "" {
+					sdkCnf.Region = "us-east-1"
+				}
+				var awsAccount *awsAccount
+				awsAccount, err = currentAwsAccount(ctx, h.logger, sdkCnf)
+				if err != nil {
+					h.logger.Error("failed to get current aws account", zap.Error(err), zap.String("sourceId", connection.SourceId))
+					return connection, err
+				}
+				metadata, err2 := NewAWSConnectionMetadata(h.logger, awsCnf, connection, *awsAccount)
+				if err2 != nil {
+					h.logger.Error("failed to get aws connection metadata", zap.Error(err2), zap.String("sourceId", connection.SourceId))
+				}
+				jsonMetadata, err2 := json.Marshal(metadata)
+				if err2 != nil {
+					return connection, err
+				}
+				connection.Metadata = jsonMetadata
+			}
 		}
 	case source.CloudAzure:
 		var azureCnf describe.AzureSubscriptionConfig
