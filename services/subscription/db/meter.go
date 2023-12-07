1e414da70f889450dca5a3782c9771b5cf521545
Saleh Khazaei <saleh.khazaei@gmail.com>
2023-12-07 14:55:28 +0400

    fix: Fixes meter

--- services/subscription/db/meter.go
+++ services/subscription/db/meter.go
@@ -1,19 +1,29 @@
 package db
 
-import "github.com/kaytu-io/kaytu-engine/services/subscription/db/model"
+import (
+	"github.com/go-errors/errors"
+	"github.com/kaytu-io/kaytu-engine/services/subscription/db/model"
+	"gorm.io/gorm"
+)
 
 func (db Database) CreateMeter(meter *model.Meter) error {
 	return db.Orm.Model(&model.Meter{}).Create(meter).Error
 }
 
 func (db Database) GetMeter(workspaceId, dateHour string, meterType model.MeterType) (*model.Meter, error) {
-	var meter *model.Meter
+	var meter model.Meter
 	err := db.Orm.Model(&model.Meter{}).
 		Where("workspace_id = ?", workspaceId).
 		Where("date_hour = ?", dateHour).
 		Where("meter_type = ?", meterType).
 		Find(&meter).Error
-	return meter, err
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &meter, nil
 }
 
 func (db Database) UpdateMeterPublished(workspaceId, dateHour string, meterType model.MeterType) error {
