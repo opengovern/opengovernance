9212788def79675de288c333328167f22fd39dbd
Parham Alvani <perhama@kaytu.io>
2023-12-15 13:41:12 +0000

    feat: add more configuration into the onboard service

--- services/onboard/config/service.go
+++ services/onboard/config/service.go
@@ -2,8 +2,11 @@ package config
 
 import "github.com/kaytu-io/kaytu-util/pkg/config"
 
-type OnboradConfig struct {
-	Postgres config.Postgres
-	Http     config.HttpServer
-	RabbitMQ config.RabbitMQ
+type OnboardConfig struct {
+	Postgres  config.Postgres
+	Http      config.HttpServer
+	RabbitMQ  config.RabbitMQ
+	Metadata  config.KaytuService
+	Inventory config.KaytuService
+	Describe  config.KaytuService
 }
