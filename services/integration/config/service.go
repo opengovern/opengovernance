e8b308bf281cb53433e2b6156dcfc71ffb0377c4
Parham Alvani <perhama@kaytu.io>
2023-12-17 19:09:03 +0000

    feat: add koanf tag into the integration service configuration

--- services/integration/config/service.go
+++ services/integration/config/service.go
@@ -3,14 +3,14 @@ package config
 import "github.com/kaytu-io/kaytu-util/pkg/koanf"
 
 type OnboardConfig struct {
-	Postgres        koanf.Postgres     `json:"postgres,omitempty"`
-	Steampipe       koanf.Postgres     `json:"steampipe,omitempty"`
-	Http            koanf.HttpServer   `json:"http,omitempty"`
-	RabbitMQ        koanf.RabbitMQ     `json:"rabbit_mq,omitempty"`
-	KMS             koanf.KMS          `json:"kms,omitempty"`
-	Metadata        koanf.KaytuService `json:"metadata,omitempty"`
-	Inventory       koanf.KaytuService `json:"inventory,omitempty"`
-	Describe        koanf.KaytuService `json:"describe,omitempty"`
-	MasterAccessKey string             `json:"master_access_key,omitempty"`
-	MasterSecretKey string             `json:"master_secret_key,omitempty"`
+	Postgres        koanf.Postgres     `json:"postgres,omitempty" koanf:"postgres"`
+	Steampipe       koanf.Postgres     `json:"steampipe,omitempty" koanf:"steampipe"`
+	Http            koanf.HttpServer   `json:"http,omitempty" koanf:"http"`
+	RabbitMQ        koanf.RabbitMQ     `json:"rabbit_mq,omitempty" koanf:"rabbit_mq"`
+	KMS             koanf.KMS          `json:"kms,omitempty" koanf:"kms"`
+	Metadata        koanf.KaytuService `json:"metadata,omitempty" koanf:"metadata"`
+	Inventory       koanf.KaytuService `json:"inventory,omitempty" koanf:"inventory"`
+	Describe        koanf.KaytuService `json:"describe,omitempty" koanf:"describe"`
+	MasterAccessKey string             `json:"master_access_key,omitempty" koanf:"master_access_key"`
+	MasterSecretKey string             `json:"master_secret_key,omitempty" koanf:"master_secret_key"`
 }
