eba35e0e2cb054bc86ec98898f2e810a622e95fa
Parham Alvani <perhama@kaytu.io>
2023-12-26 15:13:25 +0000

    feat: remove steampipe configuration from integration service

--- services/integration/config/service.go
+++ services/integration/config/service.go
@@ -4,7 +4,6 @@ import "github.com/kaytu-io/kaytu-util/pkg/koanf"
 
 type IntegrationConfig struct {
 	Postgres        koanf.Postgres     `json:"postgres,omitempty" koanf:"postgres"`
-	Steampipe       koanf.Postgres     `json:"steampipe,omitempty" koanf:"steampipe"`
 	Http            koanf.HttpServer   `json:"http,omitempty" koanf:"http"`
 	KMS             koanf.KMS          `json:"kms,omitempty" koanf:"kms"`
 	Metadata        koanf.KaytuService `json:"metadata,omitempty" koanf:"metadata"`
