a50ad569772e767bc10044958ad395a12e4609d4
Parham Alvani <perhama@kaytu.io>
2023-12-26 14:08:42 +0000

    fix: correct generate doc script

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd "$(dirname "$0")/.."
-swag fmt -g ../cmd/swagger-ui/main.go --dir "$(find {pkg,cmd} -type d | paste -d',' -s -)"
+# https://stackoverflow.com/questions/3822621/how-to-exit-if-a-command-failed
+set -eu
+set -o pipefail
+
+cd "$(dirname "$0")/.." || exit
+
+swag fmt -g ../cmd/swagger-ui/main.go --dir "$(find {pkg,cmd,services} -type d | paste -d',' -s -)"
 swag init --parseDependency -g ../cmd/swagger-ui/main.go --dir "$(find {pkg,cmd} -type d | paste -d',' -s -)" --output pkg/docs
 #context=`cat pkg/docs/tag-groups.yaml`
 #echo "$context" | cat - pkg/docs/swagger.yaml > temp && mv temp pkg/docs/swagger.yaml
