b26a5dcd473cd058b1760addba349f719981960c
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-01-26 14:31:38 +0400

    feat: add finding signals

--- pkg/types/indices.go
+++ pkg/types/indices.go
@@ -2,6 +2,7 @@ package types
 
 const (
 	FindingsIndex         = "findings"
+	FindingSignalsIndex   = "finding_signals"
 	ResourceFindingsIndex = "resource_findings"
 	StackFindingsIndex    = "stacks-findings"
 	BenchmarkSummaryIndex = "benchmark_summary"
