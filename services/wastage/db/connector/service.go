c1a772a607e2f6cc8c549c1ade0b6cba47e3ab6a
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-05-21 15:28:49 +0400

    fix: increase slow sql threshold

--- services/wastage/db/connector/service.go
+++ services/wastage/db/connector/service.go
@@ -19,11 +19,11 @@ func New(config koanf.Postgres, logLevel logger.LogLevel) (*Database, error) {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logLevel,    // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      false,       // Don't include params in the SQL log
-			Colorful:                  true,        // Disable color
+			SlowThreshold:             time.Second * 10, // Slow SQL threshold
+			LogLevel:                  logLevel,         // Log level
+			IgnoreRecordNotFoundError: true,             // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      false,            // Don't include params in the SQL log
+			Colorful:                  true,             // Disable color
 		},
 	)
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", config.Host, config.Username, config.Password, config.DB, config.Port, config.SSLMode)
