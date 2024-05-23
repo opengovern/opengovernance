080168f9118843b075c2f189d53e701e076a8124
artaasadi <arta-c@kaytu.io>
2024-05-23 19:06:04 +0200

    fix: check org before user

--- services/wastage/api/wastage/limit.go
+++ services/wastage/api/wastage/limit.go
@@ -6,13 +6,6 @@ import (
 )
 
 func checkRDSInstanceLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (bool, error) {
-	userCount, err := db.GetRDSInstanceOptimizationsCountForUser(auth0UserId)
-	if err != nil {
-		return false, err
-	}
-	if userCount < int64(UserRDSInstanceLimit) {
-		return true, nil
-	}
 	org := strings.Split(orgEmail, "@")
 	orgCount, err := db.GetRDSInstanceOptimizationsCountForOrg(org[1])
 	if err != nil {
@@ -21,17 +14,17 @@ func checkRDSInstanceLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (b
 	if orgCount < int64(OrgRDSInstanceLimit) {
 		return true, nil
 	}
+	userCount, err := db.GetRDSInstanceOptimizationsCountForUser(auth0UserId)
+	if err != nil {
+		return false, err
+	}
+	if userCount < int64(UserRDSInstanceLimit) {
+		return true, nil
+	}
 	return false, nil
 }
 
 func checkRDSClusterLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (bool, error) {
-	userCount, err := db.GetRDSClusterOptimizationsCountForUser(auth0UserId)
-	if err != nil {
-		return false, err
-	}
-	if userCount < int64(UserRDSClusterLimit) {
-		return true, nil
-	}
 	org := strings.Split(orgEmail, "@")
 	orgCount, err := db.GetRDSClusterOptimizationsCountForOrg(org[1])
 	if err != nil {
@@ -40,17 +33,17 @@ func checkRDSClusterLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (bo
 	if orgCount < int64(OrgRDSClusterLimit) {
 		return true, nil
 	}
+	userCount, err := db.GetRDSClusterOptimizationsCountForUser(auth0UserId)
+	if err != nil {
+		return false, err
+	}
+	if userCount < int64(UserRDSClusterLimit) {
+		return true, nil
+	}
 	return false, nil
 }
 
 func checkEC2InstanceLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (bool, error) {
-	userCount, err := db.GetEC2InstanceOptimizationsCountForUser(auth0UserId)
-	if err != nil {
-		return false, err
-	}
-	if userCount < int64(UserEC2InstanceLimit) {
-		return true, nil
-	}
 	org := strings.Split(orgEmail, "@")
 	orgCount, err := db.GetEC2InstanceOptimizationsCountForOrg(org[1])
 	if err != nil {
@@ -59,17 +52,17 @@ func checkEC2InstanceLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (b
 	if orgCount < int64(OrgEC2InstanceLimit) {
 		return true, nil
 	}
+	userCount, err := db.GetEC2InstanceOptimizationsCountForUser(auth0UserId)
+	if err != nil {
+		return false, err
+	}
+	if userCount < int64(UserEC2InstanceLimit) {
+		return true, nil
+	}
 	return false, nil
 }
 
 func checkEBSVolumeLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (bool, error) {
-	userCount, err := db.GetEBSVolumeOptimizationsCountForUser(auth0UserId)
-	if err != nil {
-		return false, err
-	}
-	if userCount < int64(UserEBSVolumeLimit) {
-		return true, nil
-	}
 	org := strings.Split(orgEmail, "@")
 	orgCount, err := db.GetEBSVolumeOptimizationsCountForOrg(org[1])
 	if err != nil {
@@ -78,19 +71,17 @@ func checkEBSVolumeLimit(db repo.UsageV2Repo, auth0UserId, orgEmail string) (boo
 	if orgCount < int64(OrgEBSVolumeLimit) {
 		return true, nil
 	}
+	userCount, err := db.GetEBSVolumeOptimizationsCountForUser(auth0UserId)
+	if err != nil {
+		return false, err
+	}
+	if userCount < int64(UserEBSVolumeLimit) {
+		return true, nil
+	}
 	return false, nil
 }
 
 func checkAccountsLimit(db repo.UsageV2Repo, auth0UserId, orgEmail, account string) (bool, error) {
-	userAccounts, err := db.GetAccountsForUser(auth0UserId)
-	if err != nil {
-		return false, err
-	}
-	if len(userAccounts) < int(UserAccountLimit) {
-		return true, nil
-	} else if checkAccountInList(account, userAccounts) {
-		return true, nil
-	}
 	org := strings.Split(orgEmail, "@")
 	orgAccounts, err := db.GetAccountsForOrg(org[1])
 	if err != nil {
@@ -99,6 +90,15 @@ func checkAccountsLimit(db repo.UsageV2Repo, auth0UserId, orgEmail, account stri
 	if len(orgAccounts) < int(OrgAccountLimit) {
 		return true, nil
 	} else if checkAccountInList(account, orgAccounts) {
+		return true, nil
+	}
+	userAccounts, err := db.GetAccountsForUser(auth0UserId)
+	if err != nil {
+		return false, err
+	}
+	if len(userAccounts) < int(UserAccountLimit) {
+		return true, nil
+	} else if checkAccountInList(account, userAccounts) {
 		return true, nil
 	}
 	return false, nil
