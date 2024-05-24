cce5a269d95aaa9e3d0ad463bc61e273ad9e7be1
artaasadi <arta-c@kaytu.io>
2024-05-24 16:22:11 +0200

    fix: add gorm model timestamp fields to user and organization

--- services/wastage/db/model/organization.go
+++ services/wastage/db/model/organization.go
@@ -1,10 +1,15 @@
 package model
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Organization struct {
 	OrganizationId string `gorm:"primaryKey"`
 	PremiumUntil   *time.Time
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt sql.NullTime `gorm:"index"`
 }
