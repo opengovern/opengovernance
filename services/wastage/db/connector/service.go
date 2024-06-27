c9d98d9be0b1b33a03d55cbeb41b36d3f0796b3e
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-06-27 14:31:25 +0400

    fix: gorm logging

--- services/wastage/db/connector/service.go
+++ services/wastage/db/connector/service.go
@@ -3,32 +3,23 @@ package connector
 import (
 	"fmt"
 	"github.com/kaytu-io/kaytu-util/pkg/koanf"
+	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
-	"os"
-	"time"
+	"moul.io/zapgorm2"
 )
 
 type Database struct {
 	db *gorm.DB
 }
 
-func New(config koanf.Postgres, logLevel logger.LogLevel) (*Database, error) {
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second * 10, // Slow SQL threshold
-			LogLevel:                  logLevel,         // Log level
-			IgnoreRecordNotFoundError: true,             // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      false,            // Don't include params in the SQL log
-			Colorful:                  true,             // Disable color
-		},
-	)
+func New(config koanf.Postgres, logger *zap.Logger, logLevel logger.LogLevel) (*Database, error) {
+	gormLogger := zapgorm2.New(logger)
+	gormLogger.LogMode(logLevel)
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", config.Host, config.Username, config.Password, config.DB, config.Port, config.SSLMode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: newLogger,
+		Logger: gormLogger,
 	})
 	if err != nil {
 		return nil, err
