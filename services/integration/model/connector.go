fd04a1f51752795932018521249d51e08b111225
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-08-18 17:46:10 +0400

    fix: Fixes json

--- services/integration/model/connector.go
+++ services/integration/model/connector.go
@@ -15,18 +15,18 @@ const (
 )
 
 type Connector struct {
-	Name                source.Type `gorm:"primaryKey"`
-	Label               string
-	ShortDescription    string
-	Description         string
-	Direction           source.ConnectorDirectionType `gorm:"default:'ingress'"`
-	Status              source.ConnectorStatus        `gorm:"default:'enabled'"`
-	Tier                Tier                          `gorm:"default:'Community'"`
-	Logo                string                        `gorm:"default:''"`
-	AutoOnboardSupport  bool                          `gorm:"default:false"`
-	AllowNewConnections bool                          `gorm:"default:true"`
-	MaxConnectionLimit  int                           `gorm:"default:25"`
-	Tags                datatypes.JSON                `gorm:"default:'{}'"`
+	Name                source.Type                   `gorm:"primaryKey" json:"name"`
+	Label               string                        `json:"label"`
+	ShortDescription    string                        `json:"short_description"`
+	Description         string                        `json:"description"`
+	Direction           source.ConnectorDirectionType `gorm:"default:'ingress'" json:"direction"`
+	Status              source.ConnectorStatus        `gorm:"default:'enabled'" json:"status"`
+	Tier                Tier                          `gorm:"default:'Community'" json:"tier"`
+	Logo                string                        `gorm:"default:''" json:"logo"`
+	AutoOnboardSupport  bool                          `gorm:"default:false" json:"auto_onboard_support"`
+	AllowNewConnections bool                          `gorm:"default:true" json:"allow_new_connections"`
+	MaxConnectionLimit  int                           `gorm:"default:25" json:"max_connection_limit"`
+	Tags                datatypes.JSON                `gorm:"default:'{}'" json:"tags"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
