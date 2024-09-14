cc33bf4cd49611d57a5b1decf0ea691192f70874
artaasadi <arta-c@kaytu.io>
2024-09-14 19:56:04 +0200

    fix: set odysseusSQL as default engine for query run

--- pkg/inventory/http_routes.go
+++ pkg/inventory/http_routes.go
@@ -2316,7 +2316,12 @@ func (h *HttpHandler) RunQuery(ctx echo.Context) error {
 			return err
 		}
 	} else {
-		return fmt.Errorf("invalid query engine: %s", *req.Engine)
+		resp, err = h.RunSQLSmartQuery(outputS, *req.Query, queryOutput.String(), &req)
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+			return err
+		}
 	}
 
 	span.AddEvent("information", trace.WithAttributes(
