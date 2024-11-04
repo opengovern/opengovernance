131b79dab298212b3d07a6f350a04ce590b8a7f0
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-05 00:55:39 +0330

    feat: remove all migartions

--- services/migrator/job/migration_list.go
+++ services/migrator/job/migration_list.go
@@ -1,26 +1,27 @@
 package job
 
 import (
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/analytics"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/auth"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/compliance"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/analytics"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/compliance"
 	"github.com/opengovern/opengovernance/services/migrator/job/migrations/elasticsearch"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/integration"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/inventory"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/metadata"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/resource_collection"
-	"github.com/opengovern/opengovernance/services/migrator/job/migrations/resource_info"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/inventory"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/onboard"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/resource_collection"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/resource_info"
+	// "github.com/opengovern/opengovernance/services/migrator/job/migrations/workspace"
 	"github.com/opengovern/opengovernance/services/migrator/job/types"
+		// "github.com/opengovern/opengovernance/services/migrator/job/migrations/auth"
+
 )
 
 var migrations = map[string]types.Migration{
-	"metadata":            metadata.Migration{},
-	"integration":         integration.Migration{},
-	"inventory":           inventory.Migration{},
-	"resource_collection": resource_collection.Migration{},
+	// "workspace":           workspace.Migration{},
+	// "onboard":             onboard.Migration{},
+	// "inventory":           inventory.Migration{},
+	// "resource_collection": resource_collection.Migration{},
 	"elasticsearch":       elasticsearch.Migration{},
-	"compliance":          compliance.Migration{},
-	"analytics":           analytics.Migration{},
-	"resource_info":       resource_info.Migration{},
-	"auth":                auth.Migration{},
+	// "compliance":          compliance.Migration{},
+	// "analytics":           analytics.Migration{},
+	// "resource_info":       resource_info.Migration{},
+	// "auth":              auth.Migration{},
 }
