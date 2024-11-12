8548feddbf219b33441fba1c49c589243b44c7c1
artaasadi <arta-c@kaytu.io>
2024-11-12 14:48:57 +0100

    fix: update migrator

--- services/migrator/job/migrations/analytics/model.go
+++ services/migrator/job/migrations/analytics/model.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Metric struct {
-	IntegrationTypes         []integration.Type  `json:"integrationTypeName" yaml:"IntegrationTypeName"`
+	IntegrationTypes         []integration.Type  `json:"integrationType" yaml:"integrationType"`
 	Name                     string              `json:"name" yaml:"name"`
 	Query                    string              `json:"query" yaml:"query"`
 	Tables                   []string            `json:"tables" yaml:"tables"`
@@ -19,7 +19,7 @@ type Metric struct {
 type NamedQuery struct {
 	Title            string              `json:"title" yaml:"Title"`
 	Description      string              `json:"description" yaml:"Description"`
-	IntegrationTypes []integration.Type  `json:"integrationTypeName" yaml:"IntegrationTypeName"`
+	IntegrationTypes []integration.Type  `json:"integrationType" yaml:"IntegrationType"`
 	Query            shared.Query        `json:"query" yaml:"Query"`
 	Tags             map[string][]string `json:"tags" yaml:"Tags"`
 }
