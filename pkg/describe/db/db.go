6e55fe62ba016dc26e21cec473d38dcdfbdd6ffc
artaasadi <arta-c@kaytu.io>
2024-11-15 18:00:22 +0100

    fix: add query validator automigrate model

--- pkg/describe/db/db.go
+++ pkg/describe/db/db.go
@@ -12,6 +12,6 @@ type Database struct {
 func (db Database) Initialize() error {
 	return db.ORM.AutoMigrate(&model.ComplianceJob{}, &model.ComplianceSummarizer{}, &model.ComplianceRunner{}, &model.CheckupJob{},
 		&model.AnalyticsJob{}, &model.DescribeIntegrationJob{}, &model.IntegrationDiscovery{},
-		&model.JobSequencer{}, &model.QueryRunnerJob{},
+		&model.JobSequencer{}, &model.QueryRunnerJob{}, &model.QueryValidatorJob{},
 	)
 }
