8776213a80c0592b9f9cf78e144ebb1095efde9b
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-17 21:34:44 +0330

    feat: remove information service

--- services/information/config/service.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "github.com/opengovern/og-util/pkg/koanf"
-
-type InformationConfig struct {
-	Postgres koanf.Postgres   `json:"postgres,omitempty" koanf:"postgres"`
-	Http     koanf.HttpServer `json:"http,omitempty" koanf:"http"`
-	Grpc     koanf.GrpcServer `json:"grpc,omitempty" koanf:"grpc"`
-}
