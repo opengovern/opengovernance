6517764fbb972457c96a0f00216c3029f30853d2
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-02 03:18:06 +0330

    feat: add cohere ai to integrations

---
+++ services/integration/integration-type/cohereai-project/configs/deployment.go
@@ -0,0 +1,8 @@
+package configs
+
+const (
+	DescriberDeploymentName = "og-describer-cohereai"
+	DescriberImageAddress   = "ghcr.io/opengovern/og-describer-cohereai"
+	DescriberImageTagKey    = "OG_DESCRIBER_COHEREAI_IMAGE_TAG"
+	DescriberRunCommand     = "/og-describer-cohereai"
+)
