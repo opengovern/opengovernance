75945f4cb58f8b07492c2d32746cf8e2dfc8a621
artaasadi <arta-c@kaytu.io>
2024-12-02 19:18:05 +0100

    fix: fix entraid healthcheck

--- services/integration/integration-type/entra-id-directory/entra_id_directory.go
+++ services/integration/integration-type/entra-id-directory/entra_id_directory.go
@@ -36,7 +36,7 @@ func (i *EntraIdDirectoryIntegration) HealthCheck(jsonData []byte, providerId st
 	}
 
 	return healthcheck.EntraidIntegrationHealthcheck(healthcheck.Config{
-		TenantID:     providerId,
+		TenantID:     configs.TenantID,
 		ClientID:     configs.ClientID,
 		ClientSecret: configs.ClientPassword,
 		CertPath:     "",
