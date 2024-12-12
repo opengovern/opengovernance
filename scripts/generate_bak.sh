155c262abe41f8306912ac344e1b9bf64a58154e
artaasadi <arta-c@kaytu.io>
2024-12-12 17:01:36 +0100

    fix: fix create default integrations programmatically

--- scripts/generate_bak.sh
+++ scripts/generate_bak.sh
@@ -3,7 +3,7 @@
 
 export PGPASSWORD="postgres"
 
-pg_dump integration --host "localhost" --port "5432" --username "postgres" --no-password --format=t --blobs --no-owner --no-privileges --no-comments --no-subscriptions --verbose "integration" > integration.bak
+pg_dump integration --host "localhost" --port "5432" --username "postgres" --no-password --format=t --blobs --no-owner --no-privileges --no-comments --no-subscriptions --verbose "integration" --exclude-table=integrations --exclude-table=integration_type_setups > integration.bak
 pg_dump auth --host "localhost" --port "5432" --username "postgres" --no-password --format=t --blobs --no-owner --no-privileges --no-comments --no-subscriptions --verbose "auth" > auth.bak
 pg_dump compliance --host "localhost" --port "5432" --username "postgres" --no-password --format=t --blobs --no-owner --no-privileges --no-comments --no-subscriptions --verbose "compliance" > compliance.bak
 pg_dump dex --host "localhost" --port "5432" --username "postgres" --no-password --format=t --blobs --no-owner --no-privileges --no-comments --no-subscriptions --verbose "dex" > dex.bak
