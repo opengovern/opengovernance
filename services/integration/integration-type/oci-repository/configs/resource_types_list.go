44f3965c8535ce64321c5610d5e006dd198167c2
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-12-13 16:13:21 +0400

    feat: fully add intergation

--- services/integration/integration-type/oci-repository/configs/resource_types_list.go
+++ services/integration/integration-type/oci-repository/configs/resource_types_list.go
@@ -1,5 +1,11 @@
 package configs
 
-var ResourceTypesList = []string{}
+var ResourceTypesList = []string{
+	"OCI::Image",
+	"OCI::ImageTag",
+}
 
-var TablesToResourceTypes = map[string]string{}
+var TablesToResourceTypes = map[string]string{
+	"oci_image":     "OCI::Image",
+	"oci_image_tag": "OCI::ImageTag",
+}
