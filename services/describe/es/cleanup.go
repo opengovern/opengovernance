5eaf40fb8138d3350b89a11015ebc728639bdff3
artaasadi <arta-c@kaytu.io>
2024-12-13 12:32:42 +0100

    fix: add logs for cleanup summaries

--- services/describe/es/cleanup.go
+++ services/describe/es/cleanup.go
@@ -1,14 +1,26 @@
 package es
 
 import (
-	"bytes"
 	"encoding/json"
+	es2 "github.com/opengovern/og-util/pkg/es"
 	"github.com/opengovern/og-util/pkg/opengovernance-es-sdk"
+	types2 "github.com/opengovern/opencomply/jobs/compliance-summarizer-job/types"
 	"github.com/opengovern/opencomply/pkg/types"
 	"go.uber.org/zap"
+	"golang.org/x/net/context"
 )
 
-func CleanupSummariesForJobs(logger *zap.Logger, es opengovernance.Client, jobIds []uint) error {
+type BenchmarkSummaryHit struct {
+	ID      string                  `json:"_id"`
+	Score   float64                 `json:"_score"`
+	Index   string                  `json:"_index"`
+	Type    string                  `json:"_type"`
+	Version int64                   `json:"_version,omitempty"`
+	Source  types2.BenchmarkSummary `json:"_source"`
+	Sort    []any                   `json:"sort"`
+}
+
+func CleanupSummariesForJobs(ctx context.Context, logger *zap.Logger, es opengovernance.Client, jobIds []uint) {
 	root := make(map[string]any)
 	root["query"] = map[string]any{
 		"bool": map[string]any{
@@ -23,17 +35,22 @@ func CleanupSummariesForJobs(logger *zap.Logger, es opengovernance.Client, jobId
 	}
 	query, err := json.Marshal(root)
 	if err != nil {
-		return err
+		logger.Error("Delete Summaries Error marshal query", zap.Error(err))
 	}
 
+	var res []BenchmarkSummaryHit
 	logger.Info("Query to delete summaries", zap.ByteString("query", query))
-	res, err := es.ES().DeleteByQuery([]string{types.BenchmarkSummaryIndex}, bytes.NewReader(query))
+	err = es.Search(ctx, types.BenchmarkSummaryIndex, string(query), &res)
 	if err != nil {
-		return err
+		logger.Error("Delete Summaries Error searching", zap.Error(err))
 	}
 
-	logger.Info("Delete summaries response", zap.String("resp", res.String()))
-
-	opengovernance.CloseSafe(res)
-	return nil
+	for _, h := range res {
+		keys, index := h.Source.KeysAndIndex()
+		key := es2.HashOf(keys...)
+		err = es.Delete(key, index)
+		if err != nil {
+			logger.Error("Delete Summaries Error deleting key", zap.String("key", key), zap.Error(err))
+		}
+	}
 }
--- services/describe/server.go
+++ services/describe/server.go
@@ -3816,10 +3816,7 @@ func (h HttpServer) PurgeSampleData(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete compliance runners")
 	}
 
-	err = es.CleanupSummariesForJobs(h.Scheduler.logger, h.Scheduler.es, summaryJobsIds)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "FFailed to delete summary jobsailed to delete summary jobs")
-	}
+	go es.CleanupSummariesForJobs(c.Request().Context(), h.Scheduler.logger, h.Scheduler.es, summaryJobsIds)
 
 	return c.NoContent(http.StatusOK)
 }
