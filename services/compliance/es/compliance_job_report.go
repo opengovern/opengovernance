bf004dd9be19bf4fdd01d50bfbfe7bddac0b26df
artaasadi <arta-c@kaytu.io>
2024-12-15 22:36:05 +0100

    fix: add api to get compliance job report control summary

--- services/compliance/es/compliance_job_report.go
+++ services/compliance/es/compliance_job_report.go
@@ -2,6 +2,7 @@ package es
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/opengovern/og-util/pkg/opengovernance-es-sdk"
 	"github.com/opengovern/opencomply/pkg/types"
 	"go.uber.org/zap"
@@ -23,7 +24,7 @@ type ControlViewResponse struct {
 	}
 }
 
-func GetQuickScanControlViewByJobID(ctx context.Context, logger *zap.Logger, client opengovernance.Client, jobID string, auditable bool) (*types.ComplianceJobReportControlView, error) {
+func GetJobReportControlViewByJobID(ctx context.Context, logger *zap.Logger, client opengovernance.Client, jobID string, auditable bool) (*types.ComplianceJobReportControlView, error) {
 	request := make(map[string]any)
 	request["size"] = 1
 	request["query"] = map[string]any{
@@ -62,6 +63,74 @@ func GetQuickScanControlViewByJobID(ctx context.Context, logger *zap.Logger, cli
 	return &response.Hits.Hits[0].Source, nil
 }
 
+type ControlSummaryResponse struct {
+	Hits struct {
+		Total opengovernance.SearchTotal `json:"total"`
+		Hits  []struct {
+			ID      string                                  `json:"_id"`
+			Score   float64                                 `json:"_score"`
+			Index   string                                  `json:"_index"`
+			Type    string                                  `json:"_type"`
+			Version int64                                   `json:"_version,omitempty"`
+			Source  types.ComplianceJobReportControlSummary `json:"_source"`
+			Sort    []any                                   `json:"sort"`
+		}
+	}
+}
+
+func GetJobReportControlSummaryByJobID(ctx context.Context, logger *zap.Logger, client opengovernance.Client, jobID string, auditable bool,
+	controls []string) (*types.ComplianceJobReportControlSummary, error) {
+	request := make(map[string]any)
+	request["size"] = 1
+	request["query"] = map[string]any{
+		"bool": map[string]any{
+			"must": []map[string]any{
+				{
+					"term": map[string]any{
+						"job_summary.job_id": jobID,
+					},
+				},
+				{
+					"term": map[string]any{
+						"job_summary.auditable": auditable,
+					},
+				},
+			},
+		},
+	}
+	if len(controls) > 0 {
+		sourceItems := []string{
+			"es_id",
+			"es_index",
+			"compliance_summary",
+			"job_summary",
+		}
+		for _, control := range controls {
+			sourceItems = append(sourceItems, fmt.Sprintf("controls.%s", control))
+		}
+		request["_source"] = sourceItems
+	}
+
+	b, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Info("ES Query", zap.String("index", types.ComplianceJobReportControlViewIndex), zap.String("query", string(b)))
+
+	var response ControlSummaryResponse
+	err = client.Search(ctx, types.ComplianceJobReportControlViewIndex, string(b), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(response.Hits.Hits) == 0 {
+		return nil, nil
+	}
+
+	return &response.Hits.Hits[0].Source, nil
+}
+
 type ResourceViewResponse struct {
 	Hits struct {
 		Total opengovernance.SearchTotal `json:"total"`
@@ -77,7 +146,7 @@ type ResourceViewResponse struct {
 	}
 }
 
-func GetQuickScanResourceViewByJobID(ctx context.Context, logger *zap.Logger, client opengovernance.Client, jobID string, auditable bool) (*types.ComplianceJobReportResourceView, error) {
+func GetJobReportResourceViewByJobID(ctx context.Context, logger *zap.Logger, client opengovernance.Client, jobID string, auditable bool) (*types.ComplianceJobReportResourceView, error) {
 	request := make(map[string]any)
 	request["size"] = 1
 	request["query"] = map[string]any{
