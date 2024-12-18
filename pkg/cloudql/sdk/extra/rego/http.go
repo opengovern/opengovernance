22acaf9e9529b3cee5900424708163c76118f3ae
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-12-18 20:40:16 +0400

    fix: add logs

--- pkg/cloudql/sdk/extra/rego/http.go
+++ pkg/cloudql/sdk/extra/rego/http.go
@@ -3,6 +3,7 @@ package rego
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/open-policy-agent/opa/rego"
+	"go.uber.org/zap"
 )
 
 type RegoEvaluateRequest struct {
@@ -26,11 +27,15 @@ type RegoEvaluateResponse struct {
 func (r *RegoEngine) evaluateEndpoint(c echo.Context) error {
 	req := new(RegoEvaluateRequest)
 	if err := c.Bind(req); err != nil {
+		r.logger.Error("Unable to bind request", zap.Error(err))
+		r.logger.Sync()
 		return err
 	}
 
 	results, err := r.evaluate(c.Request().Context(), req.Policies, req.Query)
 	if err != nil {
+		r.logger.Error("Unable to evaluate rego", zap.Error(err))
+		r.logger.Sync()
 		return err
 	}
 
