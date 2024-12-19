4c33a48573ee5999ab508752d67c0bf603aa136a
artaasadi <arta-c@kaytu.io>
2024-12-19 17:59:58 +0100

    fix: add github resource types

--- services/integration/integration-type/github-account/configs/resource_type_configs.go
+++ services/integration/integration-type/github-account/configs/resource_type_configs.go
@@ -3,10 +3,22 @@ package configs
 import "github.com/opengovern/opencomply/services/integration/integration-type/interfaces"
 
 var ResourceTypeConfigs = map[string]*interfaces.ResourceTypeConfiguration{
-	"Github/Container/Package": &interfaces.ResourceTypeConfiguration{
+	"Github/Container/Package": {
 		Name:            "Github/Container/Package",
 		IntegrationType: IntegrationTypeGithubAccount,
 		Description:     "",
 		Params:          []interfaces.Param{},
+	},
+	"Github/Repository": {
+		Name:            "Github/Repository",
+		IntegrationType: IntegrationTypeGithubAccount,
+		Description:     "",
+		Params: []interfaces.Param{
+			{
+				Name:        "repository_name",
+				Description: `Please provide the repo name (i.e. "internal-tools")`,
+				Required:    false,
+			},
+		},
 	},
 }
